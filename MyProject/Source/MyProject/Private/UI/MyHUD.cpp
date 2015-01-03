6fbe67ea179316ee4ea4744e9d6c9860a52ab3a0
kuangben <kuangben2010@yeah.net>
2015-01-03 15:30:29 +0800

    提交

--- MyProject/Source/MyProject/Private/UI/MyHUD.cpp
+++ MyProject/Source/MyProject/Private/UI/MyHUD.cpp
@@ -1,6 +1,5 @@
 #include "MyProject.h"
 #include "UI/MyHUD.h"
-//#include "MyHUD.h"
 
 AMyHUD::AMyHUD(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
 {
@@ -14,4 +13,13 @@ void AMyHUD::DrawHUD()
 	{
 		return;
 	}
+
+	//FString Text = LOCTEXT("WaitingForRespawn", "WAITING FOR RESPAWN").ToString();
+	//FCanvasTextItem TextItem(FVector2D::ZeroVector, FText::GetEmpty(), BigFont, HUDDark);
+	//TextItem.EnableShadow(FLinearColor::Black);
+	//TextItem.Text = FText::FromString(Text);
+	//TextItem.Scale = FVector2D(1.0f, 1.0f);
+	//TextItem.FontRenderInfo = ShadowedFont;
+	//TextItem.SetColor(FLinearColor(0.75f, 0.125f, 0.125f, 1.0f));
+	//Canvas->DrawItem(TextItem);
 }
