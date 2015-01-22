d9c4b58a0f03a9a3cdb043c10112394a733aa537
aaa <kuangben2001@163.com>
2015-01-22 19:18:43 +0800

    提交

---
+++ MyProject/Source/MyProject/Private/Game/FrameWork/Singleton.h
@@ -0,0 +1,21 @@
+﻿#ifndef _SINGLETON_H
+#define _SINGLETON_H
+
+template<class T>
+class Singleton
+{
+private:
+	static T* msSingleton;
+
+public:
+	static T* getSingletonPtr()
+	{
+		if(msSingleton == NULL)
+		{
+			msSingleton = new T();
+		}
+		return msSingleton;
+	}
+};
+
+#endif				// SINGLETON_H
