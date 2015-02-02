9abed2abe01144ee55ac0f31d26ffc18070320e2
kuangben <kuangben2010@yeah.net>
2015-02-02 23:33:18 +0800

    aaa

--- MyProject/Source/MyProject/MyProject.Build.cs
+++ MyProject/Source/MyProject/MyProject.Build.cs
@@ -31,5 +31,108 @@ public class MyProject : ModuleRules
 
         //MinFilesUsingPrecompiledHeaderOverride = 1;
         bFasterWithoutUnity = true;
+		
+		
+		
+		PublicIncludePaths.AddRange(
+			new string[] {
+				// ... add public include paths required here ...
+				Path.Combine(ThirdPartyPath, "v8", "include"),
+			}
+		);
+
+		PrivateIncludePaths.AddRange(
+			new string[] {
+				"Coupling/Private",
+				// ... add other private include paths required here ...
+				Path.Combine(ThirdPartyPath, "v8", "include"),
+			}
+		);
+
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Engine",
+				"Core",
+				// ... add other public dependencies that you statically link with here ...
+			}
+		);
+
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				// ... add private dependencies that you statically link with here ...
+			}
+		);
+
+		DynamicallyLoadedModuleNames.AddRange(
+			new string[]
+			{
+				// ... add any modules that your module loads dynamically here ...
+			}
+		);
+		
+		LoadV8(Target);
+		}
+	}
+	
+	/// <summary>
+	/// Accessor for the Module's path
+	/// </summary>
+	protected string ModulePath
+	{
+		get 
+		{ 
+			return Path.GetDirectoryName(RulesCompiler.GetModuleFilename(this.GetType().Name));
+		}
+	}
+	
+	/// <summary>
+	/// Accessor for thee ThirdParty Path.
+	/// </summary>
+	protected string ThirdPartyPath
+	{
+		get 
+		{ 
+			return Path.GetFullPath(Path.Combine(ModulePath, "..", "..", "ThirdParty"));
+		}
+	}
+
+	private bool LoadV8(TargetInfo Target)
+	{
+		if ((Target.Platform == UnrealTargetPlatform.Win64) || (Target.Platform == UnrealTargetPlatform.Win32))
+		{
+			string PlatformString;
+			string LibrariesPath = Path.Combine(ThirdPartyPath, "v8", "lib");
+
+			if (Target.Platform == UnrealTargetPlatform.Win64)
+			{
+				PlatformString = "x64";
+				LibrariesPath = Path.Combine(LibrariesPath, "Win64");
+			}
+			else
+			{
+				PlatformString = "ia32";
+				LibrariesPath = Path.Combine(LibrariesPath, "Win32");
+			}
+
+			LibrariesPath = Path.Combine(LibrariesPath, "Debug");
+
+			PublicAdditionalLibraries.Add(Path.Combine(LibrariesPath, "icui18n.lib"));
+			PublicAdditionalLibraries.Add(Path.Combine(LibrariesPath, "icuuc.lib"));
+			PublicAdditionalLibraries.Add(Path.Combine(LibrariesPath, "v8_base.lib"));
+			PublicAdditionalLibraries.Add(Path.Combine(LibrariesPath, "v8_libbase.lib"));
+			PublicAdditionalLibraries.Add(Path.Combine(LibrariesPath, "v8_nosnapshot.lib"));
+			PublicAdditionalLibraries.Add(Path.Combine(LibrariesPath, "v8_snapshot.lib"));
+
+			PublicIncludePaths.Add(Path.Combine(ThirdPartyPath, "v8", "Includes"));
+
+			Definitions.Add(string.Format("WITH_COUPLING=1"));
+
+			return true;
+		}
+
+		Definitions.Add(string.Format("WITH_COUPLING=0"));
+		return false;
 	}
 }
