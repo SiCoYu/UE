3228091b5fe535940a4fb834462495d9c68174cd
aaa <kuangben2001@163.com>
2015-02-06 13:50:48 +0800

    aaa

--- MyProject/BuildThirdParty.bat
+++ MyProject/BuildThirdParty.bat
@@ -1,12 +1,13 @@
 echo on
 set base_dir=%~dp0
 %base_dir:~0,2%
-cd %base_dir%ThirdParty\Src\build
 
 if exist %base_dir%ThirdParty\Src\build rd /s /q %base_dir%ThirdParty\Src\build
 mkdir %base_dir%ThirdParty\Src\build
 
-if exist BatPath.bat call BatPath.bat
+cd %base_dir%ThirdParty\Src\build
+
+if exist %base_dir%BatPath.bat call %base_dir%BatPath.bat
 if defined CMAKEPATH (
 	%CMAKEPATH% -G "Visual Studio 12 2013" -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo %base_dir%ThirdParty\Src
 ) else (
