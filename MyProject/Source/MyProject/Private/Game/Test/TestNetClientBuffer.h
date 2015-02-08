1bce607dc8df3b794347e12cf9fb82888937d198
kuangben <kuangben2010@yeah.net>
2015-02-08 23:00:08 +0800

    aaa

--- MyProject/Source/MyProject/Private/Game/Test/TestNetClientBuffer.h
+++ MyProject/Source/MyProject/Private/Game/Test/TestNetClientBuffer.h
@@ -73,10 +73,5 @@
 //	EXPECT_TRUE(c_buffer.count() == 0);
 //}
 //
-//int main(int argc, char *argv[])
-//{
-//	testing::InitGoogleTest(&argc, argv);
-//	return RUN_ALL_TESTS();
-//}
 
 #endif				// __CIRCULARBUFFERTEST_H
