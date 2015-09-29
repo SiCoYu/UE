b6d1ff15aa6457749bff7ae27cc433c4eb2fb3eb
aaa <kuangben2001@163.com>
2015-09-29 18:19:31 +0800

    添加一个文件

---
+++ MyProject/Content/ReadMe.txt
@@ -0,0 +1,8 @@
+资源拷贝
+(1)
+拷贝 StarterContent
+拷贝文件夹以及文件夹中的内容 E:\Self\Self\unreal\UnrealEngine-git\UnrealEngine\Samples\StarterContent 拷贝到
+E:\Self\Self\unreal\UE-GIT\UE\MyProject\Content 
+(2)
+拷贝 ThirdPerson 资源
+拷贝文件夹中的内容(不包括文件夹本身) E:\Self\Self\unreal\Sample\ThirdPerson\Content 到 E:\Self\Self\unreal\UE-GIT\UE\MyProject\Content 中
--- MyProject/Source/MyProject/Private/Game/UI/UICore/UMGForm.cpp
+++ MyProject/Source/MyProject/Private/Game/UI/UICore/UMGForm.cpp
@@ -13,6 +13,9 @@ void UUMGForm::loadUWidget(const TCHAR* name)
 
 		// warning C4996: 'StaticConstructObject': StaticConstructObject is deprecated, please use NewObject instead. For internal CoreUObject module usage, please use StaticConstructObject_Internal. Please update your code to the new API before upgrading to the next release, otherwise your project will no longer compile.
 		//m_umgWidget = Cast<UUserWidget>(StaticConstructObject(widgetClass, Ctx::getSingletonPtr()->getEngineApi()->getGameInstance()));
-		m_umgWidget = Cast<UUserWidget>(NewObject<UUserWidget>());
+		// 直接构造一个抽象类是报错误的
+		// NewObject<UUserWidget>();
+		// 构造一个类的子类，最新的 api 如下就可以了%
+		m_umgWidget = NewObject<UUserWidget>(Ctx::getSingletonPtr()->getEngineApi()->getGameInstance(), widgetClass);
 	}
 }
