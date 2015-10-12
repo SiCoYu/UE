d375fa5fa9b059566d3ea81f4a6f6655076eb953
aaa <kuangben2001@163.com>
2015-10-12 16:46:01 +0800

    修改代码

--- MyProject/Source/MyProject/Private/Game/UI/UITestCanvas/UITestCanvas.cpp
+++ MyProject/Source/MyProject/Private/Game/UI/UITestCanvas/UITestCanvas.cpp
@@ -14,15 +14,47 @@ UUITestCanvas::UUITestCanvas(const FObjectInitializer& ObjectInitializer)
 
 void UUITestCanvas::onReady()
 {
-	loadUWidget(TEXT("/Game/Blueprints/NewWidgetBlueprint.NewWidgetBlueprint_C"));
+	//loadCanvas();
+	//loadNoCanvas();
+	//loadNamedSlot();
+	loadOverlay();
 	m_umgWidget->AddToViewport();
-	EngineApi::addEventHandle((UButton*)m_umgWidget->GetWidgetFromName("Button_16"), (UObject*)this, "OnConfirmGeneric");
+	//EngineApi::addEventHandle((UButton*)m_umgWidget->GetWidgetFromName("Button_32"), (UObject*)this, "OnConfirmGeneric");
 }
 
 void UUITestCanvas::OnConfirmGeneric()
 {
-	g_pTableSys->getItem(TableID::TABLE_OBJECT, 100);
+	//g_pTableSys->getItem(TableID::TABLE_OBJECT, 100);
 
-	UPackage* tblPkg = CreatePackage(NULL, TEXT("/Game/Table/ObjectBase_client"));
-	UDataAssetLoadItem* ObjectPtr = LoadObject<UDataAssetLoadItem>(tblPkg, TEXT("/Game/Table/ObjectBase_client"));
+	//UPackage* tblPkg = CreatePackage(NULL, TEXT("/Game/Table/ObjectBase_client"));
+	//UDataAssetLoadItem* ObjectPtr = LoadObject<UDataAssetLoadItem>(tblPkg, TEXT("/Game/Table/ObjectBase_client"));
+}
+
+void UUITestCanvas::loadCanvas()
+{
+	loadUWidget(TEXT("/Game/UMG/UITestCanvas_Canvas.UITestCanvas_Canvas_C"));
+}
+
+void UUITestCanvas::loadNoCanvas()
+{
+	loadUWidget(TEXT("/Game/UMG/UITestCanvas_NoCanvas.UITestCanvas_NoCanvas_C"));
+}
+
+void UUITestCanvas::loadCanvasWithNoCanvas()
+{
+	
+}
+
+void UUITestCanvas::loadNamedSlot()
+{
+	loadUWidget(TEXT("/Game/UMG/UITestNamedSlot.UITestNamedSlot_C"));
+
+	// 目前已经有点 NamedSlot 是 "NamedSlot_28"
+	m_umgWidget->GetSlotNames(m_namedSlotArr);
+}
+
+void UUITestCanvas::loadOverlay()
+{
+	loadUWidget(TEXT("/Game/UMG/UITestOveray.UITestOveray_C"));
+	m_overlay = (UOverlay*)(m_umgWidget->GetWidgetFromName("Overlay_17"));
 }
--- MyProject/Source/MyProject/Private/Game/UI/UITestCanvas/UITestCanvas.h
+++ MyProject/Source/MyProject/Private/Game/UI/UITestCanvas/UITestCanvas.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "MyProject.h"
 #include "UMGForm.h"
 #include "UITestCanvas.generated.h"
 
@@ -7,6 +8,17 @@ UCLASS(config = Game)
 class UUITestCanvas : public UUMGForm
 {
 	GENERATED_BODY()
+
+protected:
+	TArray<FName> m_namedSlotArr;		// 所有的 NamedSlot 的名字
+	UOverlay* m_overlay;				// 一个层
+
+protected:
+	void loadCanvas();		// 加载有 Canvas 的 UI
+	void loadNoCanvas();	// 加载没有 Canvas 的 UI
+	void loadCanvasWithNoCanvas();	// 将 NoCanvas 放到 Canvas 上
+	void loadNamedSlot();	// 加载有 Named Slot 控件的 UI
+	void loadOverlay();		// 加载 Overlay 
 
 public:
 	UUITestCanvas(const FObjectInitializer& ObjectInitializer);
