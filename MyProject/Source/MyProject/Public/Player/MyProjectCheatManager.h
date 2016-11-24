ac024d90433f4b400e2f6f604a0f5a2e6e17be83
aaa <kuangben2001@163.com>
2016-11-24 14:01:02 +0800

    修改

--- MyProject/Source/MyProject/Public/Player/MyProjectCheatManager.h
+++ MyProject/Source/MyProject/Public/Player/MyProjectCheatManager.h
@@ -2,7 +2,9 @@
 
 #include "MyProjectCheatManager.generated.h"
 
-UCLASS(Within=MyProjectCheatManager)
+// E:/Self/Self/unreal/UE-GIT/UE/MyProject/Source/MyProject/Public/Player/MyProjectCheatManager.h(7): error : Parent class declared within 'PlayerController'.  Cannot override within class with 'MyProjectCheatManager' since it isn't a child
+//UCLASS(Within=UMyProjectCheatManager)
+UCLASS(Within = MyProjectPlayerController)
 class UMyProjectCheatManager : public UCheatManager
 {
 	GENERATED_UCLASS_BODY()
