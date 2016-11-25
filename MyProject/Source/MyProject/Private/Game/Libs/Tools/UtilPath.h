ade97cf204c69de7709f4e1e650041977498a0e1
aaa <kuangben2001@163.com>
2016-11-25 14:41:54 +0800

    提交

--- MyProject/Source/MyProject/Private/Game/Libs/Tools/UtilPath.h
+++ MyProject/Source/MyProject/Private/Game/Libs/Tools/UtilPath.h
@@ -20,6 +20,14 @@ public:
 	static FString GameLogDir();
 };
 
-#include "UtilPath.inl"
+//#include "UtilPath.inl"
+template<class T0, class T1>
+static std::string UtilPath::Combine(T0 param0, T1 param1)
+{
+	std::stringstream strStream;
+	strStream << param0 << "/" << "param1";
+
+	return strStream.str();
+}
 
 #endif
--- MyProject/Source/MyProject/Private/Game/Libs/Tools/UtilPath.inl
+++ MyProject/Source/MyProject/Private/Game/Libs/Tools/UtilPath.inl
@@ -1,9 +1,3 @@
-#ifndef __UtilPath_H_
-#define __UtilPath_H_
-
-#include <string>
-#include <sstream>
-
 template<class T0, class T1>
 static std::string UtilPath::Combine(T0 param0, T1 param1)
 {
@@ -12,5 +6,3 @@ static std::string UtilPath::Combine(T0 param0, T1 param1)
 
 	return strStream.str();
 }
-
-#endif
