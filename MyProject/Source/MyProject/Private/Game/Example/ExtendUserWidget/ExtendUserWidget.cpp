2a136d172f82cd9758fd64cf4966ba7cd008f6f5
codekuangben <kuangben2001@163.com>
2016-11-25 22:21:45 +0800

    提交

---
+++ MyProject/Source/MyProject/Private/Game/Example/ExtendUserWidget/ExtendUserWidget.cpp
@@ -0,0 +1,8 @@
+#include "MyProject.h"
+#include "ExtendUserWidget.h"
+
+UExtendUserWidget::UExtendUserWidget(const class FObjectInitializer& PCIP)
+	: Super(PCIP)
+{
+
+}
---
+++ MyProject/Source/MyProject/Private/Game/Example/ExtendUserWidget/ExtendUserWidget.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "Blueprint/UserWidget.h"
+#include "ExtendUserWidget.generated.h"
+
+/**
+* @brief https://wiki.unrealengine.com/Extend_UserWidget_for_UMG_Widgets
+*/
+
+UCLASS(config = Game)
+class UExtendUserWidget : public UUserWidget
+{
+	GENERATED_UCLASS_BODY()
+};
