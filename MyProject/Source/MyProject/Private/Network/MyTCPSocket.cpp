e69323a2c69bba7f200c5fa88eedc5c7345cb420
codekuangben <kuangben2001@163.com>
2016-12-11 16:53:41 +0800

    aaa

--- MyProject/Source/MyProject/Private/Network/MyTCPSocket.cpp
+++ MyProject/Source/MyProject/Private/Network/MyTCPSocket.cpp
@@ -43,24 +43,24 @@ bool MyTCPSocket::StartTCPReceiver(
 //Format IP String as Number Parts
 bool MyTCPSocket::FormatIP4ToNumber(const FString& TheIP, uint8(&Out)[4])
 {
-	////IP Formatting
-	//TheIP.Replace(TEXT(" "), TEXT(""));
+	//IP Formatting
+	TheIP.Replace(TEXT(" "), TEXT(""));
 
-	////~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	////						   IP 4 Parts
+	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	//						   IP 4 Parts
 
-	////String Parts
-	//TArray<FString> Parts;
-	////TheIP.ParseIntoArray(&Parts, TEXT("."), true);
-	//TheIP.ParseIntoArray(Parts, TEXT("."), true);
-	//if (Parts.Num() != 4)
-	//	return false;
+	//String Parts
+	TArray<FString> Parts;
+	//TheIP.ParseIntoArray(&Parts, TEXT("."), true);
+	TheIP.ParseIntoArray(Parts, TEXT("."), true);
+	if (Parts.Num() != 4)
+		return false;
 
-	////String to Number Parts
-	//for (int32 i = 0; i < 4; ++i)
-	//{
-	//	Out[i] = FCString::Atoi(*Parts[i]);
-	//}
+	//String to Number Parts
+	for (int32 i = 0; i < 4; ++i)
+	{
+		Out[i] = FCString::Atoi(*Parts[i]);
+	}
 
 	return true;
 }
@@ -143,71 +143,71 @@ FString MyTCPSocket::StringFromBinaryArray(const TArray<uint8>& BinaryArray)
 //Rama's TCP Socket Listener
 void MyTCPSocket::TCPSocketListener()
 {
-	////~~~~~~~~~~~~~
-	//if (!ConnectionSocket) return;
-	////~~~~~~~~~~~~~
+	//~~~~~~~~~~~~~
+	if (!ConnectionSocket) return;
+	//~~~~~~~~~~~~~
 
 
-	////Binary Array!
-	//TArray<uint8> ReceivedData;
+	//Binary Array!
+	TArray<uint8> ReceivedData;
 
-	//uint32 Size;
-	//while (ConnectionSocket->HasPendingData(Size))
-	//{
-	//	//ReceivedData.Init(FMath::Min(Size, 65507u));
-	//	ReceivedData.Init(0, FMath::Min(Size, 65507u));
+	uint32 Size;
+	while (ConnectionSocket->HasPendingData(Size))
+	{
+		//ReceivedData.Init(FMath::Min(Size, 65507u));
+		ReceivedData.Init(0, FMath::Min(Size, 65507u));
 
-	//	int32 Read = 0;
-	//	ConnectionSocket->Recv(ReceivedData.GetData(), ReceivedData.Num(), Read);
+		int32 Read = 0;
+		ConnectionSocket->Recv(ReceivedData.GetData(), ReceivedData.Num(), Read);
 
-	//	//GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("Data Read! %d"), ReceivedData.Num()));
-	//}
-	////~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+		//GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("Data Read! %d"), ReceivedData.Num()));
+	}
+	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-	//if (ReceivedData.Num() <= 0)
-	//{
-	//	//No Data Received
-	//	return;
-	//}
+	if (ReceivedData.Num() <= 0)
+	{
+		//No Data Received
+		return;
+	}
 
-	////VShow("Total Data read!", ReceivedData.Num());
-	//GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("Data Bytes Read ~> %d"), ReceivedData.Num()));
-
-
-	////~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	////						Rama's String From Binary Array
-	//const FString ReceivedUE4String = StringFromBinaryArray(ReceivedData);
-	////~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	//VShow("Total Data read!", ReceivedData.Num());
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("Data Bytes Read ~> %d"), ReceivedData.Num()));
 
 
-	////VShow("As String!!!!! ~>", ReceivedUE4String);
-	//GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("As String Data ~> %s"), *ReceivedUE4String));
+	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	//						Rama's String From Binary Array
+	const FString ReceivedUE4String = StringFromBinaryArray(ReceivedData);
+	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+	//VShow("As String!!!!! ~>", ReceivedUE4String);
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("As String Data ~> %s"), *ReceivedUE4String));
 }
 
 void MyTCPSocket::ConnectThirdPartySocketServer()
 {
-	//ClientSocket = ISocketSubsystem::Get(PLATFORM_SOCKETSUBSYSTEM)->CreateSocket(NAME_Stream, TEXT("default"), false);
+	ClientSocket = ISocketSubsystem::Get(PLATFORM_SOCKETSUBSYSTEM)->CreateSocket(NAME_Stream, TEXT("default"), false);
 
-	//FString address = TEXT("127.0.0.1");
-	//int32 port = 19834;
-	//FIPv4Address ip;
-	//FIPv4Address::Parse(address, ip);
+	FString address = TEXT("127.0.0.1");
+	int32 port = 19834;
+	FIPv4Address ip;
+	FIPv4Address::Parse(address, ip);
 
 
-	//TSharedRef<FInternetAddr> addr = ISocketSubsystem::Get(PLATFORM_SOCKETSUBSYSTEM)->CreateInternetAddr();
-	////addr->SetIp(ip.GetValue());
-	//addr->SetIp(ip.Value);
-	//addr->SetPort(port);
+	TSharedRef<FInternetAddr> addr = ISocketSubsystem::Get(PLATFORM_SOCKETSUBSYSTEM)->CreateInternetAddr();
+	//addr->SetIp(ip.GetValue());
+	addr->SetIp(ip.Value);
+	addr->SetPort(port);
 
-	//bool connected = ClientSocket->Connect(*addr);
+	bool connected = ClientSocket->Connect(*addr);
 }
 
 void MyTCPSocket::SendThirdPartyData()
 {
-	//FString serialized = TEXT("loadPlayer|1");
-	//TCHAR *serializedChar = serialized.GetCharArray().GetData();
-	//int32 size = FCString::Strlen(serializedChar);
-	//int32 sent = 0;
+	FString serialized = TEXT("loadPlayer|1");
+	TCHAR *serializedChar = serialized.GetCharArray().GetData();
+	int32 size = FCString::Strlen(serializedChar);
+	int32 sent = 0;
 
-	//bool successful = ClientSocket->Send((uint8*)TCHAR_TO_UTF8(serializedChar), size, sent);
+	bool successful = ClientSocket->Send((uint8*)TCHAR_TO_UTF8(serializedChar), size, sent);
 }
