dcb16b8b67b4e564fdadfbf98c593557074ca5d6
codekuangben <kuangben2001@163.com>
2016-12-16 16:12:11 +0800

    aaa

--- MyProject/Source/MyProject/Private/BasicFrame/Libs/EngineWrap/MyUEEngineInc.h
+++ MyProject/Source/MyProject/Private/BasicFrame/Libs/EngineWrap/MyUEEngineInc.h
@@ -4,6 +4,21 @@
 /**
  * @brief 最小 UE 包含头文件
  */
-#include ""
+ // #include "Platform.h"
+#include "Core.h"		// "Platform.h" 已经包含在 "Core.h" 中了
+ //#include "ICUUtilities.h"
+
+#include "UObject/Object.h" // UObject : public UObjectBaseUtility
+#include "Math/Vector.h"	// FVector
+#include "GameFramework/Actor.h"	// AActor
+#include "Components/StaticMeshComponent.h"		// UStaticMeshComponent
+#include "GenericPlatform/GenericPlatformMath.h"	// Max
+
+#include "Containers/UnrealString.h"	// FString
+#include "Engine/EngineTypes.h"	// FTimerHandle
+#include "UObject/Object.h"	// UObject
+
+#include "Templates/SharedPointer.h"
+#include "SocketSubsystem.h"
 
 #endif
---
+++ MyProject/Source/MyProject/Private/Network/MyWebSock.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "UObject/Object.h"
+
+/**
+ * @brief https://wiki.unrealengine.com/TCP_Socket_Listener,_Receive_Binary_Data_From_an_IP/Port_Into_UE4,_(Full_Code_Sample)
+ */
+
+class MyWebSock : public UObject
+{
+
+};
