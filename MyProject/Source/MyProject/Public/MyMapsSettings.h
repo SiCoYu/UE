eb7dcb1dfde5000cb80024d5e30a281743dbeaf9
aaa <kuangben2001@163.com>
2016-12-22 13:55:39 +0800

    aaa

--- MyProject/Source/MyProject/Public/MyMapsSettings.h
+++ MyProject/Source/MyProject/Public/MyMapsSettings.h
@@ -1,10 +1,15 @@
 #pragma once
 
-#include "GameMapsSettings.h"	// UGameMapsSettings
-#include "MyMapsSettings.generated.h"
+//#include "GameMapsSettings.h"	// UGameMapsSettings
+//#include "MyMapsSettings.generated.h"
 
-UCLASS()
-class UMyMapsSettings : public UGameMapsSettings
-{
-	GENERATED_BODY()
-};
+/**
+ UGameMapsSettings 不能继承，因为代码里面都是直接获取 UGameMapsSettings 的
+ const UGameMapsSettings* GameMapsSettings = GetDefault<UGameMapsSettings>();
+ */
+
+//UCLASS()
+//class UMyMapsSettings : public UGameMapsSettings
+//{
+//	GENERATED_UCLASS_BODY()
+//};
