4d6d210a83eabff3f539b60312947e5bf11aebbb
codekuangben <kuangben2001@163.com>
2016-12-25 23:18:24 +0800

    aaa

---
+++ MyProject/Source/MyProject/Private/MyDelegate/MyObjectDelegate.cpp
@@ -0,0 +1,8 @@
+#include "MyProject.h"
+#include "MyObjectDelegate.h"
+
+UMyObjectDelegate::UMyObjectDelegate(const class FObjectInitializer& PCIP)
+	: Super(PCIP)
+{
+
+}
---
+++ MyProject/Source/MyProject/Private/MyDelegate/MyObjectDelegate.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "UObject/Object.h"	// UObject
+#include "MyObjectDelegate.generated.h"
+
+UCLASS(config = Game)
+class UMyObjectDelegate : public UObject
+{
+	GENERATED_BODY()
+
+
+};
