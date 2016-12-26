f466839a5ee3f2a5471e47becfabb5168719aed5
codekuangben <kuangben2001@163.com>
2016-12-26 23:23:18 +0800

    aaa

---
+++ MyProject/Source/MyProject/Private/BasicFrame/Libs/Resource/InsResMgr/InsResMgrBase.cpp
@@ -0,0 +1,140 @@
+﻿#include "MyProject.h"
+#include "InsResMgrBase.h"
+#include "UtilStr.h"
+#include "ResItem.h"
+
+InsResMgrBase::InsResMgrBase()
+{
+	mLoadingDepth = 0;
+}
+
+void InsResMgrBase::init()
+{
+
+}
+
+void InsResMgrBase::dispose()
+{
+
+}
+
+void InsResMgrBase::loadWithResCreatedAndLoad(LoadParam* param)
+{
+	mPath2ResDic[param->mPath]->getRefCountResLoadResultNotify()->getRefCount()->incRef();
+	if (mPath2ResDic[param->mPath]->getRefCountResLoadResultNotify()->getResLoadState()->hasLoaded())
+	{
+		if (!param->mLoadEventHandle.empty())
+		{
+			param->mLoadEventHandle(mPath2ResDic[param->mPath]);        // 直接通知上层完成加载
+		}
+	}
+	else
+	{
+		if (!param->mLoadEventHandle.empty())
+		{
+			mPath2ResDic[param->mPath]->getRefCountResLoadResultNotify()->getLoadResEventDispatch()->addEventHandle(param->mLoadEventHandle);
+		}
+	}
+}
+
+void InsResMgrBase::unload(std::string path, EventDispatchDelegate loadEventHandle)
+{
+	if (UtilMap::ContainsKey(mPath2ResDic, path))
+	{
+		mPath2ResDic[path]->getRefCountResLoadResultNotify()->getLoadResEventDispatch()->removeEventHandle(loadEventHandle);
+		mPath2ResDic[path]->getRefCountResLoadResultNotify()->getRefCount()->decRef();
+		if (mPath2ResDic[path]->getRefCountResLoadResultNotify()->getRefCount()->bNoRef())
+		{
+			if (mLoadingDepth != 0)       // 如果加载深度不是 0 的，说明正在加载，不能卸载对象
+			{
+				addNoRefResID2List(path);
+			}
+			else
+			{
+				unloadNoRef(path);
+			}
+		}
+	}
+}
+
+// 添加无引用资源到 List
+void InsResMgrBase::addNoRefResID2List(std::string path)
+{
+	mZeroRefResIDList.Add(path);
+}
+
+// 卸载没有引用的资源列表中的资源
+void InsResMgrBase::unloadNoRefResFromList()
+{
+	for(std::string path : mZeroRefResIDList.getList())
+	{
+		if (mPath2ResDic[path]->getRefCountResLoadResultNotify()->getRefCount()->bNoRef())
+		{
+			unloadNoRef(path);
+		}
+	}
+	mZeroRefResIDList.Clear();
+}
+
+void InsResMgrBase::unloadNoRef(std::string path)
+{
+	mPath2ResDic[path]->unload();
+	// 卸载加载的原始资源
+	GResLoadMgr->unload(path, EventDispatchDelegate(this, &InsResMgrBase::onLoadEventHandle));
+	UtilMap::Remove(mPath2ResDic, path);
+	//UtilApi.UnloadUnusedAssets();           // 异步卸载共用资源
+}
+
+void InsResMgrBase::onLoadEventHandle(IDispatchObject* dispObj)
+{
+	ResItem* res = (ResItem*)dispObj;
+	std::string path = res->getPath();
+
+	if (UtilMap::ContainsKey(mPath2ResDic, path))
+	{
+		mPath2ResDic[path]->getRefCountResLoadResultNotify()->getResLoadState()->copyFrom(res->getRefCountResLoadResultNotify()->getResLoadState());
+		if (res->getRefCountResLoadResultNotify()->getResLoadState()->hasSuccessLoaded())
+		{
+			mPath2ResDic[path]->init(res);
+			if (mPath2ResDic[path]->getIsOrigResNeedImmeUnload())
+			{
+				// 卸载资源
+				GResLoadMgr->unload(path, EventDispatchDelegate(this, &InsResMgrBase::onLoadEventHandle));
+			}
+		}
+		else
+		{
+			mPath2ResDic[path]->failed(res);
+			GResLoadMgr->unload(path, EventDispatchDelegate(this, &InsResMgrBase::onLoadEventHandle));
+		}
+	}
+	else
+	{
+		GLogSys->log(UtilStr::Format("Path can not find {0}", path));
+		GResLoadMgr->unload(path, EventDispatchDelegate(this, &InsResMgrBase::onLoadEventHandle));
+	}
+}
+
+InsResBase* InsResMgrBase::getRes(std::string path)
+{
+	return mPath2ResDic[path];
+}
+
+// 卸载所有的资源
+void InsResMgrBase::unloadAll()
+{
+	// 卸载资源的时候保存的路径列表
+	MList<std::string> pathList;
+	for(std::pair<std::string, InsResBase*> kv : mPath2ResDic)
+	{
+		kv.second->getRefCountResLoadResultNotify()->getLoadResEventDispatch()->clearEventHandle();
+		pathList.Add(kv.first);
+	}
+
+	for(std::string path : pathList.getList())
+	{
+		unload(path, EventDispatchDelegate(this, &InsResMgrBase::onLoadEventHandle));
+	}
+
+	pathList.Clear();
+}
---
+++ MyProject/Source/MyProject/Private/BasicFrame/Libs/Resource/InsResMgr/InsResMgrBase.h
@@ -0,0 +1,161 @@
+﻿#ifndef __InsResMgrBase_H
+#define __InsResMgrBase_H
+
+#include <map>
+#include <string>
+#include "MList.h"
+#include "EventDispatchDelegate.h"
+#include "LoadParam.h"
+#include "UtilContainers.h"
+#include "RefCountResLoadResultNotify.h"
+#include "ResLoadState.h"
+#include "InsResBase.h"
+#include "ResEventDispatch.h"
+#include "RefCount.h"
+//#include "Common.h"	// TODO: 包含这个就导致 UIAssetMgr 报错 InsResMgrBase 不能识别，因此只包含必要的头文件，递归包含导致的错误
+#include "Ctx.h"
+#include "ResLoadMgr.h"
+#include "PoolSys.h"
+#include "LogSys.h"
+
+class InsResBase;
+class IDispatchObject;
+
+/**
+ * @brief 资源管理器，不包括资源加载
+ */
+class InsResMgrBase
+{
+public:
+	std::map<std::string, InsResBase*> mPath2ResDic;
+
+protected:
+	MList<std::string> mZeroRefResIDList;      // 没有引用的资源 ID 列表
+    int mLoadingDepth;          // 加载深度
+
+public:
+	InsResMgrBase();
+
+	virtual void init();
+	virtual void dispose();
+
+	template<class T>
+	T* getAndSyncLoad(std::string path, EventDispatchDelegate handle = nullptr)
+	{
+		syncLoad<T>(path, handle);
+		return (T*)getRes(path);
+	}
+
+	template<class T>
+	T* getAndAsyncLoad(std::string path, EventDispatchDelegate handle)
+	{
+		T* ret = nullptr;
+		LoadParam* param = GPoolSys->newObject<LoadParam>();
+		LocalFileSys.modifyLoadParam(path, param);
+		param.mIsLoadNeedCoroutine = true;
+		param.mIsResNeedCoroutine = true;
+		param.mLoadEventHandle = handle;
+		ret = getAndLoad<T>(param);
+		GPoolSys->deleteObj(param);
+
+		return ret;
+	}
+
+	template<class T>
+	T* getAndLoad(LoadParam* param)
+	{
+		load<T>(param);
+		return (T*)getRes(param.mPath);
+	}
+
+	// 同步加载，立马加载完成，并且返回加载的资源， syncLoad 同步加载资源不能和异步加载资源的接口同时去加载一个资源，如果异步加载一个资源，这个时候资源还没有加载完成，然后又同步加载一个资源，这个时候获取的资源是没有加载完成的，由于同步加载资源没有回调，因此即使同步加载的资源加载完成，也不可能获取加载完成事件
+	template<class T>
+	void syncLoad(std::string path, EventDispatchDelegate handle = nullptr)
+	{
+		LoadParam* param;
+		param = GPoolSys->newObject<LoadParam>();
+		param->mPath = path;
+		// param->mLoadEventHandle = onLoadEventHandle;        // 这个地方是同步加载，因此不需要回调，如果写了，就会形成死循环， InsResBase 中的 init 又会调用 onLoadEventHandle 这个函数，这个函数是外部回调的函数，由于同步加载，没有回调，因此不要设置这个 param.mLoadEventHandle = onLoadEventHandle ，内部会自动调用
+		param->mLoadEventHandle = handle;
+		param->mIsLoadNeedCoroutine = false;
+		param->mIsResNeedCoroutine = false;
+		param->mResPackType = eClassType;
+		load<T>(param);
+		GPoolSys->deleteObj(param);
+	}
+
+	template<class T>
+	T* createResItem(LoadParam* param)
+	{
+		T* ret = new T();
+		ret->getRefCountResLoadResultNotify()->getRefCount()->incRef();
+		ret->mPath = param->mPath;
+
+		ret->getRefCountResLoadResultNotify()->getLoadResEventDispatch()->addEventHandle(param->mLoadEventHandle);
+
+		return ret;
+	}
+
+protected:
+	void loadWithResCreatedAndLoad(LoadParam* param);
+
+	template<class T>
+	void loadWithResCreatedAndNotLoad(LoadParam* param, T* resItem)
+	{
+		mPath2ResDic[param->mPath] = resItem;
+		mPath2ResDic[param->mPath]->getRefCountResLoadResultNotify()->getResLoadState()->setLoading();
+		param->mLoadEventHandle = EventDispatchDelegate(this, &InsResMgrBase::onLoadEventHandle);
+		GResLoadMgr->loadAsset(param);
+	}
+
+	template<class T>
+	void loadWithNotResCreatedAndNotLoad(LoadParam* param)
+	{
+		T* resItem = createResItem<T>(param);
+		loadWithResCreatedAndNotLoad<T>(param, resItem);
+	}
+
+public:
+	// TODO:
+	//virtual void load(LoadParam* param);	// 模板函数不能使虚函数
+	template<class T>
+	void load(LoadParam* param)
+	{
+		++mLoadingDepth;
+		if (UtilMap::ContainsKey(mPath2ResDic, param->mPath))
+		{
+			loadWithResCreatedAndLoad(param);
+		}
+		else if (param->mLoadInsRes != nullptr)
+		{
+			loadWithResCreatedAndNotLoad<T>(param, (T*)(param->mLoadInsRes));
+		}
+		else
+		{
+			loadWithNotResCreatedAndNotLoad<T>(param);
+		}
+		--mLoadingDepth;
+
+		if (mLoadingDepth == 0)
+		{
+			unloadNoRefResFromList();
+		}
+	}
+
+	virtual void unload(std::string path, EventDispatchDelegate loadEventHandle);
+
+protected:
+	// 添加无引用资源到 List
+	void addNoRefResID2List(std::string path);
+    // 卸载没有引用的资源列表中的资源
+	void unloadNoRefResFromList();
+	void unloadNoRef(std::string path);
+
+public:
+	virtual void onLoadEventHandle(IDispatchObject* dispObj);
+	InsResBase* getRes(std::string path);
+    // 卸载所有的资源
+	void unloadAll();
+};
+
+#endif
