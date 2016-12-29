29afc44f47f0351a064b5533f5a04b9785b7eec8
codekuangben <kuangben2001@163.com>
2016-12-30 00:47:52 +0800

    aaa

--- MyProject/Source/MyProject/Private/MyDelegate/MyObjectDelegate.cpp
+++ MyProject/Source/MyProject/Private/MyDelegate/MyObjectDelegate.cpp
@@ -4,5 +4,14 @@
 UMyObjectDelegate::UMyObjectDelegate(const class FObjectInitializer& PCIP)
 	: Super(PCIP)
 {
+	// 方法一: 添加事件
+	//MyObjectCommonDelegate = UMyObjectCommonDelegate::CreateUObject(this, &UMyObjectDelegate::handle);
+	// 方法二: 添加事件
+	MyObjectCommonDelegate.BindUObject(this, &UMyObjectDelegate::handle);
+	MyObjectCommonDelegate.Execute(10, 10, true);
+}
 
+void UMyObjectDelegate::handle(int aaa, int bbb, bool ccc)
+{
+	
 }
--- MyProject/Source/MyProject/Private/MyDelegate/MyObjectDelegate.h
+++ MyProject/Source/MyProject/Private/MyDelegate/MyObjectDelegate.h
@@ -1,10 +1,24 @@
 #pragma once
 
 #include "UObject/Object.h"	// UObject
+#include "Delegates/DelegateCombinations.h"		// DECLARE_DELEGATE_ThreeParams
 #include "MyObjectDelegate.generated.h"
+
+// 定义一个 Delegate 类型
+DECLARE_DELEGATE_ThreeParams(UMyObjectCommonDelegate, int, int, bool);
+
+/**
+ * @brief Delegate
+ */
 
 UCLASS(config = Game)
 class UMyObjectDelegate : public UObject
 {
 	GENERATED_UCLASS_BODY()
+
+public:
+	UMyObjectCommonDelegate MyObjectCommonDelegate;
+
+public:
+	void handle(int aaa, int bbb, bool ccc);
 };
