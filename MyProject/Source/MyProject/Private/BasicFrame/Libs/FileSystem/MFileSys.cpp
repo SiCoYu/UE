6aabed5909712d650b768465b0a2290d1dd41d9f
codekuangben <kuangben2001@163.com>
2016-12-31 00:54:55 +0800

    aaa

--- MyProject/Source/MyProject/Private/BasicFrame/Libs/FileSystem/MFileSys.cpp
+++ MyProject/Source/MyProject/Private/BasicFrame/Libs/FileSystem/MFileSys.cpp
@@ -3,6 +3,8 @@
 #include "Containers/UnrealString.h"		// FString
 #include "HAL/PlatformFilemanager.h"		// FPlatformFileManager
 #include "IPlatformFileSandboxWrapper.h"	// FSandboxPlatformFile
+#include "UtilStr.h"
+#include "UtilPath.h"
 
 std::string MFileSys::msPersistentDataPath = "";
 std::string MFileSys::msStreamingAssetsPath = "";
@@ -36,59 +38,33 @@ FSandboxPlatformFile* MFileSys::getSandboxPlatformFile()
 
 void MFileSys::initFileSys()
 {
+	// Engine\Source\Runtime\OpenGLDrv\Private\OpenGLShaders.cpp
+	FString CacheFolderPath;
+#if PLATFORM_ANDROID
+	extern FString GExternalFilePath;
+	CacheFolderPath = GExternalFilePath / TEXT("ProgramBinaryCache");
+#else
+	CacheFolderPath = UtilPath::GameSavedDir() / TEXT("ProgramBinaryCache");
+#endif
 
+	MFileSys::msPersistentDataPath = UtilStr::ConvFString2StdStr(CacheFolderPath);
+
+	MFileSys::msStreamingAssetsPath = UtilStr::ConvFString2StdStr(UtilPath::GameContentDir());
 }
 
 // 获取本地可以读取的目录，但是不能写
 std::string MFileSys::getLocalReadDir()
 {
-	return "";
+	return MFileSys::msStreamingAssetsPath;
 }
 
 // 获取本地可以写的目录
 std::string MFileSys::getLocalWriteDir()
 {
-	return msPersistentDataPath;
+	return MFileSys::msPersistentDataPath;
 }
 
 void MFileSys::modifyLoadParam(std::string resPath, LoadParam* param)
 {
-//#if PKG_RES_LOAD
-//	param.mOrigPath = resPath;             // 记录原始的资源名字
-//
-//	string retPath = resPath;
-//
-//	if ("Module/AutoUpdate.prefab" == resPath)       // 自动更新模块更新还没有实现
-//	{
-//		param.mResLoadType = ResLoadType.eStreamingAssets;
-//	}
-//	else
-//	{
-//		// 获取包的名字
-//		if (Ctx.m_instance.m_pPakSys.path2PakDic.ContainsKey(resPath))
-//		{
-//			retPath = Ctx.m_instance.m_pPakSys.path2PakDic[resPath].m_pakName;
-//		}
-//
-//		if (param != null)
-//		{
-//			Ctx.m_instance.m_localFileSys.getAbsPathByRelPath(ref retPath, ref param.mResLoadType);
-//		}
-//		else
-//		{
-//			ResLoadType tmp = ResLoadType.eStreamingAssets;
-//			Ctx.m_instance.m_localFileSys.getAbsPathByRelPath(ref retPath, ref tmp);
-//		}
-//	}
-//	param.mPath = retPath;
-//	param.mPakPath = param.mPath;
-//#elif UnPKG_RES_LOAD
-//	if (param != null)
-//	{
-//		param.mResLoadType = ResLoadType.eStreamingAssets;
-//	}
-//	param.mPath = resPath;
-//#else
-//	param.mPath = resPath;
-//#endif
+
 }
--- MyProject/Source/MyProject/Private/BasicFrame/Libs/FileSystem/MFileSys.h
+++ MyProject/Source/MyProject/Private/BasicFrame/Libs/FileSystem/MFileSys.h
@@ -8,6 +8,7 @@ class FSandboxPlatformFile;
 
 /**
  * @brief 本地文件系统
+ Engine\Source\Runtime\Launch\Private\Android\AndroidJNI.cpp
  */
 class MFileSys
 {
@@ -37,6 +38,21 @@ public:
 	static std::string getLocalWriteDir();
 	// 获取真正的加载参数
 	static void modifyLoadParam(std::string resPath, LoadParam* param);
+
+public:
+	// Engine\Source\Runtime\Core\Private\Android\AndroidFile.cpp
+	// FString GFilePathBase;	getExternalStorageDirectory
+	// FString GExternalFilePath;	getExternalFilesDir
+	//const FString &GetFileBasePath()
+
+	// https://answers.unrealengine.com/questions/476998/ue4game-folder-on-external-storage-android.html
+	// I found a global variable GFilePathBase at line 865 in file \Engine\Source\Runtime\Launch\Private\Android\AndroidJNI.cpp.It is assigned to ExternalStorageDirectory(/ storage / sdcard0) of Android.And another global variable GExternalFilePath at line 926 is assigned to ExternalFilesDir(/ storage / sdcard0 / Android / data / package / files),
+
+	// 是时候弄清楚getExternalStorageDirectory()和getExternalFilesDir()的区别了 
+	// http://blog.csdn.net/nugongahou110/article/details/48154859
+
+	// Engine\Source\Runtime\Launch\Private\Android\AndroidJNI.cpp
+	// GFilePathBase = FString(nativePathString);
 };
 
 #endif
