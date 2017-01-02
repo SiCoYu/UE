58f7fd5ab2fda5b9c861c4af7b8a3e483d088c0e
codekuangben <kuangben2001@163.com>
2017-01-02 23:35:00 +0800

    aaa

--- MyProject/Source/MyProject/Private/BasicFrame/Libs/Core/Memory/MyMemory.cpp
+++ MyProject/Source/MyProject/Private/BasicFrame/Libs/Core/Memory/MyMemory.cpp
@@ -1,42 +1,42 @@
 #include "MyProject.h"
 #include "MyMemory.h"
 
-__forceinline void* operator new(size_t size)
-{
-	//return Memory::Alloc(Memory::ObjectHeap, size);
-	void* ptr = malloc(size);
-	return ptr;
-}
-
-__forceinline void* operator new(size_t size, const std::nothrow_t& noThrow)
-{
-	//return Memory::Alloc(Memory::ObjectHeap, size);
-	void* ptr = malloc(size);
-	return ptr;
-}
-
-__forceinline void* operator new[](size_t size)
-{
-	//return Memory::Alloc(Memory::ObjectArrayHeap, size);
-	void* ptr = malloc(size);
-	return ptr;
-}
-
-__forceinline void* operator new[](size_t size, const std::nothrow_t& noThrow)
-{
-	//return Memory::Alloc(Memory::ObjectArrayHeap, size);
-	void* ptr = malloc(size);
-	return ptr;
-}
-
-__forceinline void operator delete(void* ptr)
-{
-	//Memory::Free(Memory::ObjectHeap, ptr);
-	free(ptr);
-}
-
-__forceinline void operator delete[](void* ptr)
-{
-	//Memory::Free(Memory::ObjectArrayHeap, ptr);
-	free(ptr);
-}
+//void* operator new(size_t size)
+//{
+//	//return Memory::Alloc(Memory::ObjectHeap, size);
+//	void* ptr = malloc(size);
+//	return ptr;
+//}
+//
+//void* operator new(size_t size, const std::nothrow_t& noThrow)
+//{
+//	//return Memory::Alloc(Memory::ObjectHeap, size);
+//	void* ptr = malloc(size);
+//	return ptr;
+//}
+//
+//void* operator new[](size_t size)
+//{
+//	//return Memory::Alloc(Memory::ObjectArrayHeap, size);
+//	void* ptr = malloc(size);
+//	return ptr;
+//}
+//
+//void* operator new[](size_t size, const std::nothrow_t& noThrow)
+//{
+//	//return Memory::Alloc(Memory::ObjectArrayHeap, size);
+//	void* ptr = malloc(size);
+//	return ptr;
+//}
+//
+//void operator delete(void* ptr)
+//{
+//	//Memory::Free(Memory::ObjectHeap, ptr);
+//	free(ptr);
+//}
+//
+//void operator delete[](void* ptr)
+//{
+//	//Memory::Free(Memory::ObjectArrayHeap, ptr);
+//	free(ptr);
+//}
