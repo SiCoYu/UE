e9261e481e53a8c4272fa3c944f22c2587ec02c6
吕广海 <kuangben2001@163.com>
2017-01-03 19:04:43 +0800

    添加

---
+++ MyProject/Source/MyProject/Private/BasicFrame/Libs/FrameWork/SystemSetting.cpp
@@ -0,0 +1,15 @@
+﻿#include <MyProject.h>
+#include <SystemSetting.h>
+
+std::string USERNAME = "username";
+std::string PASSWORD = "password";
+
+void SystemSetting::setString(std::string key, std::string value)
+{
+    
+}
+
+std::string SystemSetting::getString(std::string key)
+{
+    return "";
+}
---
+++ MyProject/Source/MyProject/Private/BasicFrame/Libs/FrameWork/SystemSetting.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include <string>
+
+/**
+ * @brief 系统设置
+ */
+class SystemSetting
+{
+public:
+    static std::string USERNAME = "username";
+	static std::string PASSWORD = "password";
+
+public:
+    void setString(std::string key, std::string value)
+    {
+        
+    }
+
+    std::string getString(std::string key)
+    {
+
+    }
+};
