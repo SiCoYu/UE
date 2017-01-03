210eb958b09cf98a08ba86070334dc460a7d9e98
吕广海 <kuangben2001@163.com>
2017-01-03 14:30:11 +0800

    暂时去掉 new 重载

--- MyProject/Source/MyProject/Private/BasicFrame/Libs/Core/Memory/MyMemory.h
+++ MyProject/Source/MyProject/Private/BasicFrame/Libs/Core/Memory/MyMemory.h
@@ -3,20 +3,21 @@
 #include <new>	// nothrow_t
 #include <stdlib.h>	// malloc, free
 
-#ifdef new
-#undef new
-#endif
+//#ifdef new
+//#undef new
+//#endif
+//
+//#ifdef delete
+//#undef delete
+//#endif
 
-#ifdef delete
-#undef delete
-#endif
-
-__forceinline void* operator new(size_t size);
-__forceinline void* operator new(size_t size, const std::nothrow_t& noThrow);
-__forceinline void* operator new[](size_t size);
-__forceinline void* operator new[](size_t size, const std::nothrow_t& noThrow);
-__forceinline void operator delete(void* ptr);
-__forceinline void operator delete[](void* ptr);
+// error C4595: 'operator new': non-member operator new or delete functions may not be declared inline
+//__forceinline void* operator new(size_t size);
+//__forceinline void* operator new(size_t size, const std::nothrow_t& noThrow);
+//__forceinline void* operator new[](size_t size);
+//__forceinline void* operator new[](size_t size, const std::nothrow_t& noThrow);
+//__forceinline void operator delete(void* ptr);
+//__forceinline void operator delete[](void* ptr);
 
 #define my_new(type) new type
 #define my_new_array(type,size) new type[size]
