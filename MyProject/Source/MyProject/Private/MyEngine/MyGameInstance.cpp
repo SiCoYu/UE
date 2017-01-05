f1f6441d4afc8d15391a45ecfbae95bb00947c8a
codekuangben <kuangben2001@163.com>
2017-01-05 23:34:47 +0800

    aaa

--- MyProject/Source/MyProject/Private/MyEngine/MyGameInstance.cpp
+++ MyProject/Source/MyProject/Private/MyEngine/MyGameInstance.cpp
@@ -8,7 +8,8 @@
 UMyGameInstance::UMyGameInstance(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
 {
-	mAppFrame = new AppFrame();
+	// 逻辑指针不要放在构造函数中，因为脚本会调用一次构造函数，这样就会申请多个指针
+	//mAppFrame = new AppFrame();
 
 	// 这个设置等价于在 Project Setting 中的设置
 	std::string aaa = "aaa";
@@ -21,6 +22,7 @@ void UMyGameInstance::Init()
 	Super::Init();
 
 	// UGameEngine 创建后，就创建 UGameInstance，这个是除 UGameEngine 外最早初始化的地方
+	mAppFrame = new AppFrame();
 	mAppFrame->initApp();
 }
 
