469e05d05d6b82e4232377f3e98905699110db7b
codekuangben <kuangben2001@163.com>
2017-01-08 14:36:14 +0800

    aaa

--- MyProject/BuildThirdParty.bat
+++ MyProject/BuildThirdParty.bat
@@ -1,34 +1,35 @@
-:: echo on
+::echo on
+rem @echo off
 @echo off
-set base_dir=%~dp0
-%base_dir:~0,2%
+set BaseDir=%~dp0
+%BaseDir:~0,2%
 
 set GENERATOR="Visual Studio 14 2015 Win64"
 set DEVENV="D:\programfiles(x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe"
 
-if exist %base_dir%ThirdParty\Src\build rd /s /q %base_dir%ThirdParty\Src\build
-mkdir %base_dir%ThirdParty\Src\build
+if exist %BaseDir%ThirdParty\Src\build rd /s /q %BaseDir%ThirdParty\Src\build
+mkdir %BaseDir%ThirdParty\Src\build
 
-cd %base_dir%ThirdParty\Src\build
+cd %BaseDir%ThirdParty\Src\build
 
-if exist %base_dir%BatPath.bat call %base_dir%BatPath.bat
+if exist %BaseDir%BatPath.bat call %BaseDir%BatPath.bat
 if defined CMAKEPATH (
-	%CMAKEPATH% -G%GENERATOR% -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo -DCMAKE_INSTALL_PREFIX:PATH=%base_dir%ThirdParty\Src\build %base_dir%ThirdParty\Src
+	%CMAKEPATH% -G%GENERATOR% -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo -DCMAKE_INSTALL_PREFIX:PATH=%BaseDir%ThirdParty\Src\build %BaseDir%ThirdParty\Src
 ) else (
-	echo set CMAKEPATH="F:\ProgramFiles\cmake-3.7.0-win64-x64\bin\cmake.exe" > %base_dir%BatPath.bat
-	echo set DEVENV="D:\programfiles(x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" >> %base_dir%BatPath.bat
+	echo set CMAKEPATH="F:\ProgramFiles\cmake-3.7.0-win64-x64\bin\cmake.exe" > %BaseDir%BatPath.bat
+	echo set DEVENV="D:\programfiles(x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" >> %BaseDir%BatPath.bat
 	echo Error CMAKEPATH not define
 )
 
-cd %base_dir%
+cd %BaseDir%
 
-::if exist %base_dir%ThirdParty\Src\build\ThirdParty.sln (
-::	start "" %base_dir%ThirdParty\Src\build\ThirdParty.sln
+::if exist %BaseDir%ThirdParty\Src\build\ThirdParty.sln (
+::	start "" %BaseDir%ThirdParty\Src\build\ThirdParty.sln
 ::) else (
-::	echo Error %base_dir%ThirdParty\Src\build\ThirdParty.sln not exist
+::	echo Error %BaseDir%ThirdParty\Src\build\ThirdParty.sln not exist
 ::)
 
-::%DEVENV% %base_dir%ThirdParty\Src\build\ThirdParty.sln /build "RelWithDebInfo" /project "INSTALL"
-%DEVENV% %base_dir%ThirdParty\Src\build\ThirdParty.sln /build "Debug" /project "INSTALL"
+::%DEVENV% %BaseDir%ThirdParty\Src\build\ThirdParty.sln /build "RelWithDebInfo" /project "INSTALL"
+%DEVENV% %BaseDir%ThirdParty\Src\build\ThirdParty.sln /build "Debug" /project "INSTALL"
 
 pause
