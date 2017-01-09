b8199d8a4183018aab474b0871c37990d92c5dc0
codekuangben <kuangben2001@163.com>
2017-01-09 23:44:36 +0800

    aaa

--- MyProject/Content/MyAsset/Lua/MyLua/Libs/Core/ModuleLoad.lua
+++ MyProject/Content/MyAsset/Lua/MyLua/Libs/Core/ModuleLoad.lua
@@ -20,3 +20,27 @@ function MReload(path)
     MUnload(path);
     return MLoader(path);
 end
+
+--[[
+--http://lua-users.org/wiki/LuaModulesLoader
+module(..., package.seeall)
+
+local function load(modulename)
+	local errmsg = ""
+	-- Find source
+	local modulepath = string.gsub(modulename, "%.", "/")
+	for path in string.gmatch(package.path, "([^;]+)") do
+		local filename = string.gsub(path, "%?", modulepath)
+		local file = io.open(filename, "rb")
+		if file then
+			-- Compile and return the module
+			return assert(loadstring(assert(file:read("*a")), filename))
+		end
+		errmsg = errmsg.."\n\tno file '"..filename.."' (checked with custom loader)"
+	end
+	return errmsg
+end
+
+-- Install the loader so that it's called just before the normal Lua loader
+table.insert(package.loaders, 2, load)
+]]
--- MyProject/Content/MyAsset/Lua/MyLua/Module/Entry/MainEntry.lua
+++ MyProject/Content/MyAsset/Lua/MyLua/Module/Entry/MainEntry.lua
@@ -1,7 +1,7 @@
 --整个程序的入口
 --导入加载模块接口
 --自己 lua 代码中的路径都是使用点(.)分割的，并且不要加扩展名字。但是 ToLua 本地加载器使用的是斜杠(/)。加载自己 lua 代码的时候一定要使用 MLoader 去加载，这里面将路径中的点转换成斜杠。但是第一样需要包含 ModuleLoad 模块，因此使用斜杠
-require "MyLua/Libs/Core/ModuleLoad";
+require("MyLua/Libs/Core/ModuleLoad");
 --加载宏定义
 MLoader("MyLua.Libs.FrameWork.MacroDef");
 --启动调试服务器连接
