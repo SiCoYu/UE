af364f5bf4ac5ff8d3103844c34e45daea71dba1
吕广海 <kuangben2001@163.com>
2017-01-12 18:32:11 +0800

    aaa

--- MyProject/Source/MyProjectEditor/Private/MyEngine/Example/AnimNode/AnimGraphNode_TranslateWith.cpp
+++ MyProject/Source/MyProjectEditor/Private/MyEngine/Example/AnimNode/AnimGraphNode_TranslateWith.cpp
@@ -24,25 +24,29 @@ FString UAnimGraphNode_TranslateWith::GetNodeCategory() const
 {
 	return FString("Skeletal Controls");
 }
+
 FText UAnimGraphNode_TranslateWith::GetControllerDescription() const
 {
 	return LOCTEXT("TranslateWithNode", "Translate With");
 }
 
-
 //Node Tooltip
-FString UAnimGraphNode_TranslateWith::GetTooltip() const
+//FString UAnimGraphNode_TranslateWith::GetTooltip() const
+//{
+//	return TEXT("The Translate WIth control allows the Target Bone to follow the Translation of the Source Bone on all Specified Axis");
+//}
+
+FText UAnimGraphNode_TranslateWith::GetTooltipText() const
 {
-	return TEXT("The Translate WIth control allows the Target Bone to follow the Translation of the Source Bone on all Specified Axis");
+	FString tmp = "aaa";
+	return FText::FromString(tmp);
 }
 
 //Node Title(Needs to be updated now since node is much more versatile)
 FText UAnimGraphNode_TranslateWith::GetNodeTitle(ENodeTitleType::Type TitleType) const
 {
-
 	FFormatNamedArguments Args;
 	Args.Add(TEXT("ControllerDescription"), GetControllerDescription());
-
 
 	if (TitleType == ENodeTitleType::ListView)
 	{
@@ -53,8 +57,9 @@ FText UAnimGraphNode_TranslateWith::GetNodeTitle(ENodeTitleType::Type TitleType)
 		//Args.Add(TEXT("SourceBoneName"), FText::FromName(Node.SourceBone.BoneName));
 		//Args.Add(TEXT("TargetBoneName"), FText::FromName(Node.TargetBone.BoneName));
 		//return FText::Format(LOCTEXT("AnimGraphNode_TranslateWith_Title", "{ControllerDescription}\nSource Bone: {SourceBoneName}\nTarget Bone: {TargetBoneName}"), Args);
-	}
 
+		return FText::Format(LOCTEXT("AnimGraphNode_TranslateWith_Title", "{ControllerDescription}"), Args);
+	}
 }
 
 //Node Output Pin(Output is in Component Space, Change at own RISK!)
--- MyProject/Source/MyProjectEditor/Private/MyEngine/Example/AnimNode/AnimGraphNode_TranslateWith.h
+++ MyProject/Source/MyProjectEditor/Private/MyEngine/Example/AnimNode/AnimGraphNode_TranslateWith.h
@@ -10,8 +10,10 @@
 //(2)
 //Hello, in case anyone is still having this issue, I included "Editor/UnrealEd/Public/Editor.h" before AnimGraphNode_Base.h and it worked for me.
 
+// 使用这两行
 #include "Editor/UnrealEd/Public/Editor.h"
 #include "AnimGraphNode_Base.h"	// UAnimGraphNode_Base
+// 不使用这两行
 //#include "AnimGraphDefinitions.h"
 //#include "Kismet2/BlueprintEditorUtils.h"
 //#include "AnimNode_TranslateWith.h"
@@ -34,7 +36,9 @@ class UAnimGraphNode_TranslateWith : public UAnimGraphNode_Base
 public:
 	// UEdGraphNode interface
 	virtual FText GetNodeTitle(ENodeTitleType::Type TitleType) const override;
-	virtual FString GetTooltip() const override;
+	// warning C4996: 'UEdGraphNode::GetTooltip': UEdGraphNode::GetTooltip() is deprecated, instead use GetTooltipText(), which returns localized text. Please update your code to the new API before upgrading to the next release, otherwise your project will no longer compile.
+	//virtual FString GetTooltip() const override;
+	virtual FText GetTooltipText() const override;
 	virtual FString GetNodeCategory() const override;
 	virtual FLinearColor GetNodeTitleColor() const override;
 	virtual void CreateOutputPins() override;
