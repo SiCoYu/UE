42b493c8c5567c51f874c5194fa5d96bd2fdb10c
codekuangben <kuangben2001@163.com>
2017-01-13 22:55:08 +0800

    aaa

--- Android/Ndk/Test/BuildClassFiles.bat
+++ Android/Ndk/Test/BuildClassFiles.bat
@@ -7,4 +7,14 @@ set BaseDir=%~dp0
 cd %BaseDir%
 %BaseDir:~0,2%
 
-D:\ProgramFiles\Java\jdk1.8.0_65\bin\javac.exe -d %BaseDir%Java\JavaClassFiles -sourcepath %BaseDir% Com\Ndk\Test\JniClient.java
+set JAVA_SRC_ROOT=%BaseDir%Java
+cd %JAVA_SRC_ROOT%
+
+set JAVA_CLASS_OUTPATH=%BaseDir%Java\JavaClassFiles
+
+set INIT_PATH=%BaseDir%
+
+rem D:\ProgramFiles\Java\jdk1.8.0_65\bin\javac.exe -d %JAVA_CLASS_OUTPATH% -sourcepath %BaseDir%Java Com\Ndk\Test\JniClient.java
+D:\ProgramFiles\Java\jdk1.8.0_65\bin\javac.exe -d %JAVA_CLASS_OUTPATH% Com\Ndk\Test\JniClient.java
+
+cd %INIT_PATH%
--- Android/Ndk/Test/Com/Ndk/Test/JniClient.java
+++
@@ -1,7 +0,0 @@
-package Com.Ndk.Test;
-
-public class JniClient 
-{
-    static public native String AddStr(String strA, String strB);
-    static public native int AddInt(int a, int b);
-}
