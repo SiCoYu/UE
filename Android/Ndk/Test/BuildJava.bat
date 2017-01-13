217be3e615f699bf32c62e2940f6f2f11e6efb32
codekuangben <kuangben2001@163.com>
2017-01-14 00:07:55 +0800

    aaa

--- Android/Ndk/Test/BuildJava.bat
+++ Android/Ndk/Test/BuildJava.bat
@@ -1,5 +1,4 @@
 :: echo on
-echo on
 rem @echo off
 set BASE_DIR=%~dp0
 %BASE_DIR:~0,2%
@@ -7,17 +6,17 @@ set BASE_DIR=%~dp0
 cd %BASE_DIR%
 %BASE_DIR:~0,2%
 
-set JAVA_CLASS_PATH=%BASE_DIR%Java\JavaClass
-set ANDROID_CLASS_PATH=F:\ProgramFiles\android\sdk\platforms\android-25\android.jar
+set JAVA_SRC_ROOT=%BASE_DIR%Java
+cd %JAVA_SRC_ROOT%
 
-cd %JAVA_CLASS_PATH%
-
-set JAVA_JNI_OUTPATH=%BASE_DIR%Src\JniHeader
+set JAVA_CLASS_OUTPATH=%BASE_DIR%Java\JavaClass
 
 set INIT_PATH=%BASE_DIR%
 
-rem D:\ProgramFiles\Java\jdk1.8.0_65\bin\javah.exe -jni -classpath %ANDROID_CLASS_PATH%;%JAVA_CLASS_PATH% Com\Ndk\Test\JniClient
-D:\ProgramFiles\Java\jdk1.8.0_65\bin\javah.exe -jni -classpath %ANDROID_CLASS_PATH%;%JAVA_CLASS_PATH% -d %JAVA_JNI_OUTPATH% Com.Ndk.Test.JniClient
+set JDK_PATH=D:\ProgramFiles\Java\jdk1.8.0_65
+
+rem %JDK_PATH%\bin\javac.exe -d %JAVA_CLASS_OUTPATH% -sourcepath %BASE_DIR%Java Com\Ndk\Test\JniClient.java
+rem %JDK_PATH%\bin\javac.exe -d %JAVA_CLASS_OUTPATH% -sourcepath %JAVA_SRC_ROOT% -s %JAVA_SRC_ROOT% Com\Ndk\Test\JniClient.java
+%JDK_PATH%\bin\javac.exe -d %JAVA_CLASS_OUTPATH% -sourcepath %JAVA_SRC_ROOT% -s %JAVA_SRC_ROOT% @%JAVA_SRC_ROOT%/Sources.list
 
 cd %INIT_PATH%
-
