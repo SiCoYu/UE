505e4cb95cfc249c5a89b22b53f99f75dd411d09
codekuangben <kuangben2001@163.com>
2017-01-13 22:06:55 +0800

    aaa

--- MyProject/ThirdParty/Src/lua-5.3.1/Android.mk
+++ MyProject/ThirdParty/Src/lua-5.3.1/Android.mk
@@ -18,68 +18,7 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH)/src
 
 #LOCAL_EXPORT_C_INCLUDES := $(LOCAL_C_INCLUDES)
 
-LOCAL_SRC_FILES := \
-	src/lapi.c \
-	src/lapi.h \
-	src/lauxlib.c \
-	src/lauxlib.h \
-	src/lbaselib.c \
-	src/lbitlib.c \
-	src/lcode.c \
-	src/lcode.h \
-	src/lcorolib.c \
-	src/lctype.c \
-	src/lctype.h \
-	src/ldblib.c \
-	src/ldebug.c \
-	src/ldebug.h \
-	src/ldo.c \
-	src/ldo.h \
-	src/ldump.c \
-	src/lfunc.c \
-	src/lfunc.h \
-	src/lgc.c \
-	src/lgc.h \
-	src/linit.c \
-	src/liolib.c \
-	src/llex.c \
-	src/llex.h \
-	src/llimits.h \
-	src/lmathlib.c \
-	src/lmem.c \
-	src/lmem.h \
-	src/loadlib.c \
-	src/lobject.c \
-	src/lobject.h \
-	src/lopcodes.c \
-	src/lopcodes.h \
-	src/loslib.c \
-	src/lparser.c \
-	src/lparser.h \
-	src/lprefix.h \
-	src/lstate.c \
-	src/lstate.h \
-	src/lstring.c \
-	src/lstring.h \
-	src/lstrlib.c \
-	src/ltable.c \
-	src/ltable.h \
-	src/ltablib.c \
-	src/ltm.c \
-	src/ltm.h \
-	src/lua.c \
-	src/lua.h \
-	src/lua.hpp \
-	#src/luac.c \
-	src/luaconf.h \
-	src/lualib.h \
-	src/lundump.c \
-	src/lundump.h \
-	src/lutf8lib.c \
-	src/lvm.c \
-	src/lvm.h \
-	src/lzio.c \
-	src/lzio.h
+LOCAL_SRC_FILES := src/lapi.c src/lauxlib.c src/lbaselib.c src/lbitlib.c src/lcode.c src/lcorolib.c src/lctype.c src/ldblib.c src/ldebug.c src/ldo.c src/ldo.h src/ldump.c src/lfunc.c src/lgc.c src/lgc.h src/linit.c src/liolib.c src/llex.c src/llex.h src/lmathlib.c src/lmem.c src/loadlib.c src/lobject.c src/lopcodes.c src/loslib.c src/lparser.c src/lstate.c src/lstring.c src/lstrlib.c src/ltable.c src/ltable.h src/ltablib.c src/ltm.c src/lua.c src/lundump.c src/lutf8lib.c src/lvm.c src/lzio.c 
 
 LOCAL_CFLAGS += -DTEST_DEF -fPIC
 #LOCAL_CXXFLAGS += -DGL_GLEXT_PROTOTYPES
