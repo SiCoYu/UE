2558dec1a0704fc8beb0c2721f5e77e96c436886
codekuangben <kuangben2001@163.com>
2017-01-13 23:44:22 +0800

    aaa

--- Android/Ndk/Test/BuildJniHeaderFiles.bat
+++ Android/Ndk/Test/BuildJniHeaderFiles.bat
@@ -1,4 +1,5 @@
 :: echo on
+echo on
 rem @echo off
 set BASE_DIR=%~dp0
 %BASE_DIR:~0,2%
@@ -6,13 +7,15 @@ set BASE_DIR=%~dp0
 cd %BASE_DIR%
 %BASE_DIR:~0,2%
 
-set JAVA_CLASS_PATH=%BASE_DIR%Java\JavaClassFiles
+set JAVA_CLASS_PATH=%BASE_DIR%Java\JavaClass
 set ANDROID_CLASS_PATH=F:\ProgramFiles\android\sdk\platforms\android-25\android.jar
 
 cd %JAVA_CLASS_PATH%
 
 set INIT_PATH=%BASE_DIR%
 
-D:\ProgramFiles\Java\jdk1.8.0_65\bin\javah.exe -jni -classpath %ANDROID_CLASS_PATH%;%JAVA_CLASS_PATH% Com\Ndk\Test\JniClient
+rem D:\ProgramFiles\Java\jdk1.8.0_65\bin\javah.exe -jni -classpath %ANDROID_CLASS_PATH%;%JAVA_CLASS_PATH% Com\Ndk\Test\JniClient
+D:\ProgramFiles\Java\jdk1.8.0_65\bin\javah.exe -jni -classpath %ANDROID_CLASS_PATH%;%JAVA_CLASS_PATH% Com.Ndk.Test.JniClient
 
 cd %INIT_PATH%
+
