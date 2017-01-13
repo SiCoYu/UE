60eb7545603d3f1bb843df067a3433593203c4d2
codekuangben <kuangben2001@163.com>
2017-01-13 22:46:48 +0800

    aaa

--- Android/Ndk/Test/BuildClassFiles.bat
+++ Android/Ndk/Test/BuildClassFiles.bat
@@ -1,9 +1,10 @@
 :: echo on
-@echo off
+echo on
+rem @echo off
 set BaseDir=%~dp0
 %BaseDir:~0,2%
 
 cd %BaseDir%
 %BaseDir:~0,2%
 
-D:\ProgramFiles\Java\jdk1.8.0_65\bin\javac.exe -d %BaseDir%Java\JavaClassFiles -sourcepath %BaseDir%Java Com\Ndk\Test\JniClient.java
+D:\ProgramFiles\Java\jdk1.8.0_65\bin\javac.exe -d %BaseDir%Java\JavaClassFiles -sourcepath %BaseDir% Com\Ndk\Test\JniClient.java
