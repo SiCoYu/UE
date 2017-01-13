4a318a98bdd3150b98996c9adeac45adb42b44ff
codekuangben <kuangben2001@163.com>
2017-01-13 23:53:26 +0800

    aaa

---
+++ Android/Ndk/Test/BuildJava.bat
@@ -0,0 +1,23 @@
+:: echo on
+echo on
+rem @echo off
+set BASE_DIR=%~dp0
+%BASE_DIR:~0,2%
+
+cd %BASE_DIR%
+%BASE_DIR:~0,2%
+
+set JAVA_CLASS_PATH=%BASE_DIR%Java\JavaClass
+set ANDROID_CLASS_PATH=F:\ProgramFiles\android\sdk\platforms\android-25\android.jar
+
+cd %JAVA_CLASS_PATH%
+
+set JAVA_JNI_OUTPATH=%BASE_DIR%Src\JniHeader
+
+set INIT_PATH=%BASE_DIR%
+
+rem D:\ProgramFiles\Java\jdk1.8.0_65\bin\javah.exe -jni -classpath %ANDROID_CLASS_PATH%;%JAVA_CLASS_PATH% Com\Ndk\Test\JniClient
+D:\ProgramFiles\Java\jdk1.8.0_65\bin\javah.exe -jni -classpath %ANDROID_CLASS_PATH%;%JAVA_CLASS_PATH% -d %JAVA_JNI_OUTPATH% Com.Ndk.Test.JniClient
+
+cd %INIT_PATH%
+
---
+++ Android/Ndk/Test/ReadMe.txt
@@ -0,0 +1,5 @@
+生成 jni 文件
+(1)
+生成 java 的 class 文件，执行 BuildClassFiles.bat
+(2)
+生成 class 的 h 文件，执行 BuildJniHeaderFiles.bat
