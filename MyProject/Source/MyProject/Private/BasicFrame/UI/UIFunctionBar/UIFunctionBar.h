15da3899f6ae1fb6d27cbd99756f47d10422c295
codekuangben <kuangben2001@163.com>
2017-01-14 23:37:51 +0800

    aaa

--- MyProject/Source/MyProject/Private/BasicFrame/UI/UIFunctionBar/UIFunctionBar.h
+++ MyProject/Source/MyProject/Private/BasicFrame/UI/UIFunctionBar/UIFunctionBar.h
@@ -15,5 +15,8 @@ public:
 	virtual void onReady() override;
 
 public:
+	// UFunction* UObject::FindFunction( FName InName ) const
+	// 绑定到 UMG 中的函数，一定要添加到虚拟机中，因为需要从 FindFunction 接口去虚拟机中去查找这个接口，如果不添加，就不能查找到
+	UFUNCTION(BlueprintCallable, Category = "MyButton")
 	void onTestButtonTouch();
 };
