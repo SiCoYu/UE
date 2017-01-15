74cc0f13f890ad0400e579cf5e56ceb367e46c00
codekuangben <kuangben2001@163.com>
2017-01-15 16:54:54 +0800

    aaa

--- MyProject/Source/MyProject/Private/BasicFrame/UI/UIFunctionBar/UIFunctionBar.cpp
+++ MyProject/Source/MyProject/Private/BasicFrame/UI/UIFunctionBar/UIFunctionBar.cpp
@@ -24,8 +24,15 @@ void UUIFunctionBar::onTestButtonTouch()
 	FLatentActionInfo info;
 	info.ExecutionFunction = "onLevelLoaded";
 	info.CallbackTarget = this;
+	info.UUID = 0;
+	// Linkage 一定要有值，具体参考
+	// Engine\Source\Runtime\Engine\Private\LevelStreaming.cpp
+	// Response.FinishAndTriggerIf(bIsOperationFinished, LatentInfo.ExecutionFunction, LatentInfo.Linkage, LatentInfo.CallbackTarget);
+	// Engine\Source\Runtime\Engine\Private\LatentActionManager.cpp
+	// if (LinkInfo.LinkID != INDEX_NONE)
+	info.Linkage = 0;
 
-	EngineApi::LoadStreamLevel(GEngineData->getMainActor(), "TestMap2", true, true, info);
+	EngineApi::LoadStreamLevel(GEngineData->getMainActor(), "/Game/MyAsset/Map/TestMap2", true, true, info);
 	//EngineApi::SetClientTravel(EngineApi::GetWorld(), TEXT("/Game/MyAsset/Map/TestMap2"), TRAVEL_Absolute);
 	//EngineApi::ServerTravel(TEXT("/Game/MyAsset/Map/TestMap2?game=TDM?listen"));
 }
