7d94b265d069b1268ace40a2a7bd98093352eb50
codekuangben <kuangben2001@163.com>
2017-01-15 22:50:09 +0800

    aaa

--- MyProject/Source/MyProject/Private/MyEngine/MyWorldSettings.cpp
+++ MyProject/Source/MyProject/Private/MyEngine/MyWorldSettings.cpp
@@ -2,11 +2,13 @@
 #include "MyWorldSettings.h"
 #include "MyPhysicsCollisionHandler.h"
 #include "MyGameMode.h"
+#include "MyFlyGameMode.h"
 
 AMyWorldSettings::AMyWorldSettings(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
 {
 	// 每一个关卡物理碰撞处理
-	PhysicsCollisionHandlerClass = UMyPhysicsCollisionHandler::StaticClass();
-	DefaultGameMode = AMyGameMode::StaticClass();	// 如果没有设置，就是用引擎设置的
+	this->PhysicsCollisionHandlerClass = UMyPhysicsCollisionHandler::StaticClass();
+	//this->DefaultGameMode = AMyGameMode::StaticClass();	// 如果没有设置，就是用引擎设置的
+	this->DefaultGameMode = AMyFlyGameMode::StaticClass();
 }
