42d7ea7b905856300fa7cfeb20d278928511b205
codekuangben <kuangben2001@163.com>
2017-01-16 23:19:34 +0800

    aaa

---
+++ MyProject/Source/MyProject/Private/MyEngine/MyFly/AI/MyFlyAvoidanceManager.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "AI/Navigation/AvoidanceManager.h"		// UAvoidanceManager
+#include "MyAvoidanceManager.generated.h"
+
+UCLASS(config = Engine, Blueprintable)
+class UMyAvoidanceManager : public UAvoidanceManager
+{
+	GENERATED_UCLASS_BODY()
+};
