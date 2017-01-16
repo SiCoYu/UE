243418202f14e73c691cce3986a0dcce8361fb6e
codekuangben <kuangben2001@163.com>
2017-01-16 21:56:44 +0800

    aaa

---
+++ MyProject/Source/MyProject/Private/MyEngine/MyFly/Frame/MyFlyGameInstance.cpp
@@ -0,0 +1,36 @@
+﻿#include "MyProject.h"
+#include "MyFlyGameInstance.h"
+
+#include "MyFlyOnlineSession.h"
+
+UMyFlyGameInstance::UMyFlyGameInstance(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	// 逻辑指针不要放在构造函数中，因为脚本会调用一次构造函数，这样就会申请多个指针
+	//mAppFrame = new AppFrame();
+
+	// 这个设置等价于在 Project Setting 中的设置
+	//std::string aaa = "aaa";
+	//GetDefault<UGeneralProjectSettings>()->ProjectDisplayedTitle = UtilStr::ConvStdStr2FText(aaa);
+	//GetDefault<UInputSettings>()->DefaultViewportMouseCaptureMode = EMouseCaptureMode::CaptureDuringMouseDown;
+}
+
+void UMyFlyGameInstance::Init()
+{
+	Super::Init();
+}
+
+void UMyFlyGameInstance::Shutdown()
+{
+	Super::Shutdown();
+}
+
+void UMyFlyGameInstance::FinishDestroy()
+{
+	Super::FinishDestroy();
+}
+
+TSubclassOf<UOnlineSession> UMyFlyGameInstance::GetOnlineSessionClass()
+{
+	return UMyFlyOnlineSession::StaticClass();
+}
