ada16f3abde8b5fbfcfe68141ad10f8a410e1d39
codekuangben <kuangben2001@163.com>
2017-01-21 08:55:32 +0800

    aaa

--- MyProject/Source/MyProject/Private/MyEngine/MyFly/Frame/MyFlyGameMode.cpp
+++ MyProject/Source/MyProject/Private/MyEngine/MyFly/Frame/MyFlyGameMode.cpp
@@ -1,13 +1,14 @@
 #include "MyProject.h"
 #include "MyFlyGameMode.h"
 #include "MyFlyPawn.h"
+#include "MyFlyPlayerController.h"
 
 AMyFlyGameMode::AMyFlyGameMode()
 {
 	// set default pawn class to our flying pawn
 	this->DefaultPawnClass = AMyFlyPawn::StaticClass();
 	this->HUDClass = AHUD::StaticClass();
-	this->PlayerControllerClass = APlayerController::StaticClass();
+	this->PlayerControllerClass = AMyFlyPlayerController::StaticClass();
 	this->GameStateClass = AGameState::StaticClass();
 	this->PlayerStateClass = APlayerState::StaticClass();
 	this->SpectatorClass = ASpectatorPawn::StaticClass();
