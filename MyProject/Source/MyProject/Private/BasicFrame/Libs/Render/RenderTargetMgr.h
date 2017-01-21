ea7540130a39ef9791127669dcced9334c7e0d3a
codekuangben <kuangben2001@163.com>
2017-01-21 16:05:39 +0800

    aaa

---
+++ MyProject/Source/MyProject/Private/BasicFrame/Libs/Render/RenderTargetMgr.h
@@ -0,0 +1,10 @@
+#pragma once
+
+/**
+ * @brief RenderTargetMgr
+ * @ref Unreal Engine 4 RenderTarget制作Live Camera效果: http://www.chinaar.com/UE4/1816.html
+ */
+class RenderTargetMgr
+{
+
+};
--- MyProject/Source/MyProject/Private/MyEngine/MyBase/Anim/MyAnimInstanceBase.h
+++ MyProject/Source/MyProject/Private/MyEngine/MyBase/Anim/MyAnimInstanceBase.h
@@ -6,12 +6,10 @@
 
 /**
  * @brief MyAnimInstanceBase
- * @ref ue4中动画通知的几种方式
- * @url http://blog.csdn.net/yangxuan0261/article/details/52097917
- * @ref ue4-骨骼动画相关(换装、重定向、曲线、变形) 
- * @url http://blog.csdn.net/yangxuan0261/article/details/54631067
- * @ref ue4-混合动画及动画状态机 
- * @url http://blog.csdn.net/yangxuan0261/article/details/54630949
+ * @ref ue4中动画通知的几种方式: http://blog.csdn.net/yangxuan0261/article/details/52097917
+ * @ref ue4-骨骼动画相关(换装、重定向、曲线、变形): http://blog.csdn.net/yangxuan0261/article/details/54631067
+ * @ref ue4-混合动画及动画状态机: http://blog.csdn.net/yangxuan0261/article/details/54630949
+ * @brief 虚幻4 捏脸和换装系统实现: http://www.chinaar.com/UE4/1817.html ,Demo下载 链接：http://pan.baidu.com/s/1eQRwhGI 密码：5ji2
  */
 
 UCLASS(config = Game)
