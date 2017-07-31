8684fa5dc3404c3833d955ed786dea7573f14e0f
lvguanghai <kuangben2001@163.com>
2017-07-31 20:21:31 +0800

    aaa

--- Project/MyProject/Tool/Build/Windows/Android/BuildBin.bat
+++ Project/MyProject/Tool/Build/Windows/Android/BuildBin.bat
@@ -23,23 +23,23 @@ cd %BASE_DIR%
 
 cd %UE4_BATCH_FILES_FULL_PATH%
 
-%RUNUAT_APP_FULL_PATH% BuildCookRun -project=%MYPROJECT_CONFIG_FULL_PATH% -noP4 -platform=Win64 -clientconfig=Development -serverconfig=Development -cook -allmaps -build -stage -pak -archive -archivedirectory=%BIN_OUT_FULL_PATH%
+%RUNUAT_APP_FULL_PATH% BuildCookRun -project=%MYPROJECT_CONFIG_FULL_PATH% -noP4 -platform=%BIN_APP_PLATFORM% -clientconfig=Development -serverconfig=Development -cook -allmaps -build -stage -pak -archive -archivedirectory=%BIN_OUT_FULL_PATH%
 
 cd %BASE_DIR%
 
-if not exist %APK_ORIG_FULL_PATH% (
-	echo "%APK_ORIG_FULL_PATH% is not exist"
+if not exist %Bin_APP_ORIG_FULL_PATH% (
+	echo "%Bin_APP_ORIG_FULL_PATH% is not exist"
 ) else (
-	REM 先把上次打出来的apk删除掉
-	if exist %APK_ORIG_FULL_PATH% (
-		echo "delete apk %APK_ORIG_FULL_PATH%"
-		del %APK_ORIG_FULL_PATH%
+	REM delete APP
+	if exist %Bin_APP_ORIG_FULL_PATH% (
+		echo "delete apk %Bin_APP_ORIG_FULL_PATH%"
+		del %Bin_APP_ORIG_FULL_PATH%
 	) else (
-		echo "apk output path=%APK_ORIG_FULL_PATH%"
+		echo "apk output path=%Bin_APP_ORIG_FULL_PATH%"
 	)
 	
-	echo "%APK_ORIG_FULL_PATH% is exist"
-	ren %APK_ORIG_FULL_PATH% Plane.apk
+	echo "%Bin_APP_ORIG_FULL_PATH% is exist"
+	ren %Bin_APP_ORIG_FULL_PATH% Plane.apk
 )
 
 rem pause
