ca39b538473f7e53da202b85a7f76e010c3731ca
codekuangben <kuangben2001@163.com>
2017-08-12 23:17:42 +0800

    aaa

---
+++ Project/MyProject/MyProject/Source/MyProject/Private/MyEngine/UnitTest/TestStaticConstructorAndLoad/AMTestStaticConstructorAndLoad.h
@@ -0,0 +1,15 @@
+#pragma once
+
+/**
+ * @brief Creating component subobjects and attaching them to the actor's hierarchy can also be done inside of the constructor. When spawning an actor, its components will be cloned from the CDO. In order to ensure that components are always created, destroyed, and properly garbage-collected, a pointer to every component created in the constructor should be stored in a UPROPERTY of the owning class. 
+ * @ref https://docs.unrealengine.com/latest/INT/Programming/UnrealArchitecture/Reference/Classes/index.html
+ */
+
+UCLASS()
+class AMTestUProperty : public AActor
+{
+    GENERATED_BODY()
+    public:
+    UPROPERTY()
+    UDrawSphereComponent* StaticMesh ;
+};
