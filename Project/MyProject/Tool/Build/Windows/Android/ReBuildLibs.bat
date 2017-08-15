62fc4e8eb736d66647ec0a59ef4f4dc204245040
lvguanghai <kuangben2001@163.com>
2017-08-15 13:08:06 +0800

    aaa

---
+++ Project/MyProject/Tool/Build/Windows/Android/ReBuildLibs.bat
@@ -0,0 +1,31 @@
+::echo on
+
+echo off
+
+if not defined BuildParam (
+	if exist BuildParam.bat (
+		call BuildParam.bat
+	)
+)
+
+if exist CreateDir.bat (
+	call CreateDir.bat
+)
+
+set BASE_DIR=%~dp0
+echo "BASE_DIR="%BASE_DIR%
+
+set DRIVER=%BASE_DIR:~0,2%
+echo "DRIVER="%DRIVER%
+
+%DRIVER%
+cd %BASE_DIR%
+
+cd %UE4_BATCH_FILES_FULL_PATH%
+
+:: ref VS IDE [Solution Configurations] -- Debug Editor, [Solution Platforms] -- Win64
+:: UE4Editor : UE4 + Editor
+::..\..\Build\BatchFiles\Rebuild.bat UE4Editor Win64 Debug -waitmutex
+%REBUILD_APP_FULL_PATH% UE4Game Android Debug -waitmutex
+
+pause
