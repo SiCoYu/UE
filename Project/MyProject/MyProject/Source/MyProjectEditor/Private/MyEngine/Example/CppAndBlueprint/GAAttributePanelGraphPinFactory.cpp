fca0df27529b1ad9daba75fcc6dad61a485f54c6
codekuangben <kuangben2001@163.com>
2017-09-02 15:04:18 +0800

    aaa

--- Project/MyProject/MyProject/Source/MyProjectEditor/Private/MyEngine/Example/CppAndBlueprint/GAAttributePanelGraphPinFactory.cpp
+++
@@ -1,3 +0,0 @@
-//#include "MyProjectEditor.h"
-#include "GAAttributePanelGraphPinFactory.h"
-#include "GAAttributePin.h"
--- Project/MyProject/MyProject/Source/MyProjectEditor/Private/MyEngine/MyBase/MyUnrealEdEngineBase.cpp
+++
@@ -1,9 +0,0 @@
-//#include "MyProjectEditor.h"
-// 第一行一定要包含自己的头文件
-#include "MyUnrealEdEngineBase.h"
-
-UMyUnrealEdEngineBase::UMyUnrealEdEngineBase(const class FObjectInitializer& PCIP)
-	: Super(PCIP)
-{
-
-}
