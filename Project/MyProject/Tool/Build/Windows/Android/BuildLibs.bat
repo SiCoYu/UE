2df12244e0df7ff29d3fa9575712b58fdfb25b87
lvguanghai <kuangben2001@163.com>
2017-09-04 19:06:36 +0800

    提交编译

--- Project/MyProject/Tool/Build/Windows/Android/BuildLibs.bat
+++ Project/MyProject/Tool/Build/Windows/Android/BuildLibs.bat
@@ -23,8 +23,12 @@ cd %BASE_DIR%
 
 cd %UE4_BATCH_FILES_FULL_PATH%
 
+:: Engine\Source\Programs\UnrealBuildTool\Android\UEBuildAndroid.cs
+:: -Architectures \ -GPUArchitectures
+:: Engine\Source\Programs\UnrealBuildTool\Android\AndroidToolChain.cs
+:: -armv7 常亮
 :: ref VS IDE [Solution Configurations] -- Debug Editor, [Solution Platforms] -- Win64
 :: UE4Editor : UE4 + Editor
-%BUILD_APP_FULL_PATH% UE4Game Android Debug -waitmutex
+%BUILD_APP_FULL_PATH% UE4Game Android Debug -waitmutex -Architectures=-armv7 -GPUArchitectures=-es2
 
 pause
