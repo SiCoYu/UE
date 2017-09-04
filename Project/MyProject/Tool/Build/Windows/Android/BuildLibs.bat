8902b7a6ff908a394f29ac6074249fb4fa263231
lvguanghai <kuangben2001@163.com>
2017-09-04 19:08:57 +0800

    aaa

--- Project/MyProject/Tool/Build/Windows/Android/BuildLibs.bat
+++ Project/MyProject/Tool/Build/Windows/Android/BuildLibs.bat
@@ -26,7 +26,7 @@ cd %UE4_BATCH_FILES_FULL_PATH%
 :: Engine\Source\Programs\UnrealBuildTool\Android\UEBuildAndroid.cs
 :: -Architectures \ -GPUArchitectures
 :: Engine\Source\Programs\UnrealBuildTool\Android\AndroidToolChain.cs
-:: -armv7 常量
+:: -armv7 常量定义
 :: ref VS IDE [Solution Configurations] -- Debug Editor, [Solution Platforms] -- Win64
 :: UE4Editor : UE4 + Editor
 %BUILD_APP_FULL_PATH% UE4Game Android Debug -waitmutex -Architectures=-armv7 -GPUArchitectures=-es2
