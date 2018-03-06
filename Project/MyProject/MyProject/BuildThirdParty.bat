c5ba292d2e3583f4060050e8a11af58c6f633a6b
kuangben2001 <kuangben2001@163.com>
2018-03-06 18:48:23 +0800

    aaa

--- Project/MyProject/MyProject/BuildThirdParty.bat
+++ Project/MyProject/MyProject/BuildThirdParty.bat
@@ -1,35 +1,35 @@
 ::echo on
 rem @echo off
 @echo off
-set BaseDir=%~dp0
-%BaseDir:~0,2%
+set BASE_DIR=%~dp0
+%BASE_DIR:~0,2%
 
 set GENERATOR="Visual Studio 14 2015 Win64"
-set DEVENV="D:\programfiles(x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe"
+set DEVENV="C:\ProgramFiles (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe"
 
-if exist %BaseDir%ThirdParty\Src\build rd /s /q %BaseDir%ThirdParty\Src\build
-mkdir %BaseDir%ThirdParty\Src\build
+if exist %BASE_DIR%ThirdParty\Src\build rd /s /q %BASE_DIR%ThirdParty\Src\build
+mkdir %BASE_DIR%ThirdParty\Src\build
 
-cd %BaseDir%ThirdParty\Src\build
+cd %BASE_DIR%ThirdParty\Src\build
 
-if exist %BaseDir%BatPath.bat call %BaseDir%BatPath.bat
+if exist %BASE_DIR%BatPath.bat call %BASE_DIR%BatPath.bat
 if defined CMAKEPATH (
-	%CMAKEPATH% -G%GENERATOR% -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo -DCMAKE_INSTALL_PREFIX:PATH=%BaseDir%ThirdParty\Src\build %BaseDir%ThirdParty\Src
+	%CMAKEPATH% -G%GENERATOR% -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo -DCMAKE_INSTALL_PREFIX:PATH=%BASE_DIR%ThirdParty\Src\build %BASE_DIR%ThirdParty\Src
 ) else (
-	echo set CMAKEPATH="F:\ProgramFiles\cmake-3.7.0-win64-x64\bin\cmake.exe" > %BaseDir%BatPath.bat
-	echo set DEVENV="D:\programfiles(x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" >> %BaseDir%BatPath.bat
+	echo set CMAKEPATH="F:\ProgramFiles\cmake-3.7.0-win64-x64\bin\cmake.exe" > %BASE_DIR%BatPath.bat
+	echo set DEVENV="D:\programfiles(x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" >> %BASE_DIR%BatPath.bat
 	echo Error CMAKEPATH not define
 )
 
-cd %BaseDir%
+cd %BASE_DIR%
 
-::if exist %BaseDir%ThirdParty\Src\build\ThirdParty.sln (
-::	start "" %BaseDir%ThirdParty\Src\build\ThirdParty.sln
+::if exist %BASE_DIR%ThirdParty\Src\build\ThirdParty.sln (
+::	start "" %BASE_DIR%ThirdParty\Src\build\ThirdParty.sln
 ::) else (
-::	echo Error %BaseDir%ThirdParty\Src\build\ThirdParty.sln not exist
+::	echo Error %BASE_DIR%ThirdParty\Src\build\ThirdParty.sln not exist
 ::)
 
-::%DEVENV% %BaseDir%ThirdParty\Src\build\ThirdParty.sln /build "RelWithDebInfo" /project "INSTALL"
-%DEVENV% %BaseDir%ThirdParty\Src\build\ThirdParty.sln /build "Debug" /project "INSTALL"
+::%DEVENV% %BASE_DIR%ThirdParty\Src\build\ThirdParty.sln /build "RelWithDebInfo" /project "INSTALL"
+%DEVENV% %BASE_DIR%ThirdParty\Src\build\ThirdParty.sln /build "Debug" /project "INSTALL"
 
 pause
