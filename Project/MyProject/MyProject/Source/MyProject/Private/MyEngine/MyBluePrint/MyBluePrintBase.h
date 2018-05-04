0ab17f10983dbcad9873d96ebc8fc1765ed36080
kuangben2001 <kuangben2001@163.com>
2018-05-04 14:04:00 +0800

    aaa

--- Project/MyProject/MyProject/Source/MyProject/Private/MyEngine/MyBluePrint/MyBluePrintBase.h
+++ Project/MyProject/MyProject/Source/MyProject/Private/MyEngine/MyBluePrint/MyBluePrintBase.h
@@ -3,7 +3,10 @@
 #include "UObject/Object.h"
 #include "MyBluePrintBase.generated.h"
 
-UCLASS(config = Game)
+// 这样 BluePrint 是查找不到的
+//UCLASS(config = Game)
+// 这样才能查找到
+UCLASS(Blueprintable)
 class UMyBluePrintBase : public UObject
 {
 	GENERATED_BODY()
