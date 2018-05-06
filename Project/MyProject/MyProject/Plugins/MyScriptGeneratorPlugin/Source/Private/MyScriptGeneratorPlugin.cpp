acd29165db93d69610096a00511221e6c1d9543f
codekuangben <kuangben2001@163.com>
2018-05-06 21:02:37 +0800

    aaa

---
+++ Project/MyProject/MyProject/Plugins/MyScriptGeneratorPlugin/Source/Private/MyScriptGeneratorPlugin.cpp
@@ -0,0 +1,108 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#include "CoreMinimal.h"
+#include "IProjectManager.h"
+#include "Features/IModularFeatures.h"
+#include "UniquePtr.h"
+#include "ProjectDescriptor.h"
+
+DEFINE_LOG_CATEGORY(LogScriptGenerator);
+
+class FMyScriptGeneratorPlugin : public IScriptGeneratorPlugin
+{
+	/** Specialized script code generator */
+	TUniquePtr<FScriptCodeGeneratorBase> CodeGenerator;
+
+	/** IModuleInterface implementation */
+	virtual void StartupModule() override;
+	virtual void ShutdownModule() override;
+
+	/** IScriptGeneratorPlugin interface */
+	virtual FString GetGeneratedCodeModuleName() const override { return TEXT("MyScriptGeneratorPlugin"); }
+	virtual bool ShouldExportClassesForModule(const FString& ModuleName, EBuildModuleType::Type ModuleType, const FString& ModuleGeneratedIncludeDirectory) const override;
+	virtual bool SupportsTarget(const FString& TargetName) const override;
+	virtual void Initialize(const FString& RootLocalPath, const FString& RootBuildPath, const FString& OutputDirectory, const FString& IncludeBase) override;
+	virtual void ExportClass(UClass* Class, const FString& SourceHeaderFilename, const FString& GeneratedHeaderFilename, bool bHasChanged) override;
+	virtual void FinishExport() override;
+	virtual FString GetGeneratorName() const override;
+};
+
+IMPLEMENT_MODULE( FScriptGeneratorPlugin, ScriptGeneratorPlugin )
+
+
+void FMyScriptGeneratorPlugin::StartupModule()
+{
+	// Register ourselves as an editor feature
+	IModularFeatures::Get().RegisterModularFeature(TEXT("MyScriptGeneratorPlugin"), this);
+}
+
+void FMyScriptGeneratorPlugin::ShutdownModule()
+{
+	CodeGenerator.Reset();
+
+	// Unregister our feature
+	IModularFeatures::Get().UnregisterModularFeature(TEXT("MyScriptGeneratorPlugin"), this);
+}
+
+FString FMyScriptGeneratorPlugin::GetGeneratorName() const
+{
+	
+}
+
+void FScriptGeneratorPlugin::Initialize(const FString& RootLocalPath, const FString& RootBuildPath, const FString& OutputDirectory, const FString& IncludeBase)
+{
+	UE_LOG(LogScriptGenerator, Log, TEXT("Output directory: %s"), *OutputDirectory);
+}
+
+bool FMyScriptGeneratorPlugin::ShouldExportClassesForModule(const FString& ModuleName, EBuildModuleType::Type ModuleType, const FString& ModuleGeneratedIncludeDirectory) const
+{ 
+	bool bCanExport = (ModuleType == EBuildModuleType::EngineRuntime || ModuleType == EBuildModuleType::GameRuntime);
+	if (bCanExport)
+	{
+		// Only export functions from selected modules
+		static struct FSupportedModules
+		{
+			TArray<FString> SupportedScriptModules;
+			FSupportedModules()
+			{
+				GConfig->GetArray(TEXT("Plugins"), TEXT("ScriptSupportedModules"), SupportedScriptModules, GEngineIni);
+			}
+		} SupportedModules;
+		bCanExport = SupportedModules.SupportedScriptModules.Num() == 0 || SupportedModules.SupportedScriptModules.Contains(ModuleName);
+	}
+	return bCanExport;
+}
+
+void FMyScriptGeneratorPlugin::ExportClass(UClass* Class, const FString& SourceHeaderFilename, const FString& GeneratedHeaderFilename, bool bHasChanged)
+{
+	CodeGenerator->ExportClass(Class, SourceHeaderFilename, GeneratedHeaderFilename, bHasChanged);
+}
+
+void FMyScriptGeneratorPlugin::FinishExport()
+{
+	CodeGenerator->FinishExport();
+}
+
+bool FMyScriptGeneratorPlugin::SupportsTarget(const FString& TargetName) const
+{
+	// We only support the target if it explicitly enables the required script plugin in its uproject file
+	bool bSupportsTarget = false;
+	if (FPaths::IsProjectFilePathSet())
+	{
+		FProjectDescriptor ProjectDescriptor;
+		FText OutError;
+		if (ProjectDescriptor.Load(FPaths::GetProjectFilePath(), OutError))
+		{
+			for (auto& PluginDescriptor : ProjectDescriptor.Plugins)
+			{
+				// For your own script plugin you might want to change the hardcoded name here to something else
+				if (PluginDescriptor.bEnabled && PluginDescriptor.Name == TEXT("MyScriptGeneratorPlugin"))
+				{
+					bSupportsTarget = true;
+					break;
+				}
+			}
+		}
+	}
+	return bSupportsTarget;
+}
