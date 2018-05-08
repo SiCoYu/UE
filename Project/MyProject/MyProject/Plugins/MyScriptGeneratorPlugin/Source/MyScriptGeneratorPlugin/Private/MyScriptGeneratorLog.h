8dced832a41ebed0510898c2b56b8c538f343ec3
codekuangben <kuangben2001@163.com>
2018-05-08 23:17:08 +0800

    aaaa

--- Project/MyProject/MyProject/Plugins/MyScriptGeneratorPlugin/Source/MyScriptGeneratorPlugin/Private/MyScriptGeneratorLog.h
+++
@@ -1,9 +0,0 @@
-// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
-#pragma once
-
-#include "CoreMinimal.h"
-#include "IMyScriptGeneratorPlugin.h"
-
-DECLARE_LOG_CATEGORY_EXTERN(LogMyScriptGenerator, Log, All);
-// You should place include statements to your module's private header files here.  You only need to
-// add includes for headers that are used in most of your module's source files though.
--- Project/MyProject/MyProject/Plugins/MyScriptGeneratorPlugin/Source/MyScriptGeneratorPlugin/Private/MyScriptGeneratorPlugin.cpp
+++ Project/MyProject/MyProject/Plugins/MyScriptGeneratorPlugin/Source/MyScriptGeneratorPlugin/Private/MyScriptGeneratorPlugin.cpp
@@ -1,104 +1,20 @@
 // Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
 
-#include "CoreMinimal.h"
-#include "MyScriptGeneratorLog.h"
-#include "IProjectManager.h"
-#include "Features/IModularFeatures.h"
-#include "UniquePtr.h"
-#include "ProjectDescriptor.h"
+#include "MyScriptGeneratorPlugin.h"
 
-DEFINE_LOG_CATEGORY(LogMyScriptGenerator);
+#define LOCTEXT_NAMESPACE "FMyScriptGeneratorPluginModule"
 
-class FMyScriptGeneratorPlugin : public IMyScriptGeneratorPlugin
+void FMyScriptGeneratorPluginModule::StartupModule()
 {
-	/** IModuleInterface implementation */
-	virtual void StartupModule() override;
-	virtual void ShutdownModule() override;
-
-	/** IScriptGeneratorPlugin interface */
-	virtual FString GetGeneratedCodeModuleName() const override { return TEXT("MyScriptGeneratorPlugin"); }
-	virtual bool ShouldExportClassesForModule(const FString& ModuleName, EBuildModuleType::Type ModuleType, const FString& ModuleGeneratedIncludeDirectory) const override;
-	virtual bool SupportsTarget(const FString& TargetName) const override;
-	virtual void Initialize(const FString& RootLocalPath, const FString& RootBuildPath, const FString& OutputDirectory, const FString& IncludeBase) override;
-	virtual void ExportClass(UClass* Class, const FString& SourceHeaderFilename, const FString& GeneratedHeaderFilename, bool bHasChanged) override;
-	virtual void FinishExport() override;
-	virtual FString GetGeneratorName() const override;
-};
-
-IMPLEMENT_MODULE( FMyScriptGeneratorPlugin, MyScriptGeneratorPlugin)
-
-
-void FMyScriptGeneratorPlugin::StartupModule()
-{
-	// Register ourselves as an editor feature
-	IModularFeatures::Get().RegisterModularFeature(TEXT("ScriptGenerator"), this);
+	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
 }
 
-void FMyScriptGeneratorPlugin::ShutdownModule()
+void FMyScriptGeneratorPluginModule::ShutdownModule()
 {
-	// Unregister our feature
-	IModularFeatures::Get().UnregisterModularFeature(TEXT("ScriptGenerator"), this);
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
 }
 
-FString FMyScriptGeneratorPlugin::GetGeneratorName() const
-{
-
-}
-
-void FMyScriptGeneratorPlugin::Initialize(const FString& RootLocalPath, const FString& RootBuildPath, const FString& OutputDirectory, const FString& IncludeBase)
-{
-	UE_LOG(LogMyScriptGenerator, Log, TEXT("Output directory: %s"), *OutputDirectory);
-}
-
-bool FMyScriptGeneratorPlugin::ShouldExportClassesForModule(const FString& ModuleName, EBuildModuleType::Type ModuleType, const FString& ModuleGeneratedIncludeDirectory) const
-{ 
-	bool bCanExport = (ModuleType == EBuildModuleType::EngineRuntime || ModuleType == EBuildModuleType::GameRuntime);
-	if (bCanExport)
-	{
-		// Only export functions from selected modules
-		static struct FSupportedModules
-		{
-			TArray<FString> SupportedScriptModules;
-			FSupportedModules()
-			{
-				GConfig->GetArray(TEXT("Plugins"), TEXT("ScriptSupportedModules"), SupportedScriptModules, GEngineIni);
-			}
-		} SupportedModules;
-		bCanExport = SupportedModules.SupportedScriptModules.Num() == 0 || SupportedModules.SupportedScriptModules.Contains(ModuleName);
-	}
-	return bCanExport;
-}
-
-void FMyScriptGeneratorPlugin::ExportClass(UClass* Class, const FString& SourceHeaderFilename, const FString& GeneratedHeaderFilename, bool bHasChanged)
-{
+#undef LOCTEXT_NAMESPACE
 	
-}
-
-void FMyScriptGeneratorPlugin::FinishExport()
-{
-	
-}
-
-bool FMyScriptGeneratorPlugin::SupportsTarget(const FString& TargetName) const
-{
-	// We only support the target if it explicitly enables the required script plugin in its uproject file
-	bool bSupportsTarget = false;
-	if (FPaths::IsProjectFilePathSet())
-	{
-		FProjectDescriptor ProjectDescriptor;
-		FText OutError;
-		if (ProjectDescriptor.Load(FPaths::GetProjectFilePath(), OutError))
-		{
-			for (auto& PluginDescriptor : ProjectDescriptor.Plugins)
-			{
-				// For your own script plugin you might want to change the hardcoded name here to something else
-				if (PluginDescriptor.bEnabled && PluginDescriptor.Name == TEXT("MyScriptGeneratorPlugin"))
-				{
-					bSupportsTarget = true;
-					break;
-				}
-			}
-		}
-	}
-	return bSupportsTarget;
-}
+IMPLEMENT_MODULE(FMyScriptGeneratorPluginModule, MyScriptGeneratorPlugin)
