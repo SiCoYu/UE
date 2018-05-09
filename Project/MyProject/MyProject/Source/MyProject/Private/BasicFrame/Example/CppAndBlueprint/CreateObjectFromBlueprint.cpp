8c0761daad6dd8231fa8ae6c91b13bc6154a6bbf
codekuangben <kuangben2001@163.com>
2018-05-09 21:10:41 +0800

    aaa

--- Project/MyProject/MyProject/Source/MyProject/Private/BasicFrame/Example/CppAndBlueprint/CreateObjectFromBlueprint.cpp
+++ Project/MyProject/MyProject/Source/MyProject/Private/BasicFrame/Example/CppAndBlueprint/CreateObjectFromBlueprint.cpp
@@ -9,6 +9,8 @@ UObject* UCreateObjectFromBlueprint::NewObjectFromBlueprint(UObject* WorldContex
 	// UE4 4.17 warning C4996: 'UEngine::GetWorldFromContextObject': GetWorldFromContextObject(Object) and GetWorldFromContextObject(Object, boolean) are replaced by GetWorldFromContextObject(Object, Enum) or GetWorldFromContextObjectChecked(Object) Please update your code to the new API before upgrading to the next release, otherwise your project will no longer compile.
 	//UWorld* World = GEngine->GetWorldFromContextObject(WorldContextObject);
 	UWorld* World = GEngine->GetWorldFromContextObject(WorldContextObject, EGetWorldErrorMode::LogAndReturnNull);
+
+	// 只传递一个参数会宕机，因为模板是要生成的类，第一个参数是外部容器，如果使用请参照 UMyBluePrintToCppFunctionLibrary::newObjectFromBlueprint
 	UObject* tempObject = NewObject<UObject>(UC);
 
 	return tempObject;
