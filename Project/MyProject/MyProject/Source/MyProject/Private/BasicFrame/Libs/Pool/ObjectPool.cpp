ed787154b99a4d0782f69012e6cbad7c07cc36e9
codekuangben <kuangben2001@163.com>
2018-05-20 22:34:22 +0800

    aaa

---
+++ Project/MyProject/MyProject/Source/MyProject/Private/BasicFrame/Libs/Pool/ObjectPool.cpp
@@ -0,0 +1,26 @@
+﻿#include "MyProject.h"
+#include "IRecycleObject.h"
+#include "PoolSys.h"
+
+PoolSys::PoolSys()
+{
+	this->mPoolList = new LockList<IRecycleObject*>("PoolList");
+}
+
+void PoolSys::init()
+{
+
+}
+
+void PoolSys::dispose()
+{
+
+}
+
+void PoolSys::deleteObj(IRecycleObject* obj)
+{
+	//if (this->mPoolList.IndexOf(obj) == -1)
+	//{
+	//    this->mPoolList.Add(obj);
+	//}
+}
---
+++ Project/MyProject/MyProject/Source/MyProject/Private/BasicFrame/Libs/Pool/ObjectPoolGroup.h
@@ -0,0 +1,50 @@
+﻿#ifndef __PoolSys_H
+#define __PoolSys_H
+
+#include "LockList.h"
+
+class IRecycleObject;
+
+/**
+ * @brief 对象池
+ */
+class PoolSys
+{
+protected:
+	LockList<IRecycleObject*>* mPoolList;
+
+public:
+	PoolSys();
+
+	void init();
+	void dispose();
+
+	template<class T>
+	T* newObject()
+	{
+		//T retObj = default(T);
+		//// 查找
+		//int idx = 0;
+		//for(idx = 0; idx < mPoolList.Count; ++idx)
+		//{
+		//    if (typeof(T) == mPoolList[idx].GetType())
+		//    {
+		//        retObj = (T)mPoolList[idx];
+		//        mPoolList.RemoveAt(idx);
+		//        MethodInfo myMethodInfo = retObj.GetType().GetMethod("resetDefault");
+		//        if (myMethodInfo != null)
+		//        {
+		//            myMethodInfo.Invoke(retObj, null);
+		//        }
+		//        return retObj;
+		//    }
+		//}
+
+		T* retObj = new T();
+		return retObj;
+	}
+
+	void deleteObj(IRecycleObject* obj);
+};
+
+#endif
