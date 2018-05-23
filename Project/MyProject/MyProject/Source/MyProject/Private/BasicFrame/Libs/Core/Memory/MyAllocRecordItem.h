375ad84563f6b65cf49d8f9fbd5bfc2a0b2735ec
codekuangben <kuangben2001@163.com>
2018-05-23 22:54:47 +0800

    aaa

---
+++ Project/MyProject/MyProject/Source/MyProject/Private/BasicFrame/Libs/Core/Memory/MyAllocRecordItem.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <new>	// nothrow_t
+#include <stdlib.h>	// malloc, free
+
+//#ifdef new
+//#undef new
+//#endif
+//
+//#ifdef delete
+//#undef delete
+//#endif
+
+// error C4595: 'operator new': non-member operator new or delete functions may not be declared inline
+//__forceinline void* operator new(size_t size);
+//__forceinline void* operator new(size_t size, const std::nothrow_t& noThrow);
+//__forceinline void* operator new[](size_t size);
+//__forceinline void* operator new[](size_t size, const std::nothrow_t& noThrow);
+//__forceinline void operator delete(void* ptr);
+//__forceinline void operator delete[](void* ptr);
+
+#define my_new(type) new type
+#define my_new_array(type,size) new type[size]
+#define my_delete(ptr) delete ptr
+#define my_delete_array(ptr) delete[] ptr
