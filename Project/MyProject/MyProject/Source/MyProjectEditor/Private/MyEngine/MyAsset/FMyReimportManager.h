2d6ddf39a15f9a05fe8ca6335b41c7ece8c12bee
codekuangben <kuangben2001@163.com>
2018-06-10 19:10:39 +0800

    aaa

---
+++ Project/MyProject/MyProject/Source/MyProjectEditor/Private/MyEngine/MyAsset/FMyReimportManager.h
@@ -0,0 +1,22 @@
+#pragma once
+
+//#include "PlatformDefine.h"
+
+// UE4 4.17: error : UCLASS inside this preprocessor block will be skipped
+//#ifdef MY_WITH_EDITOR
+
+#include "Editor/EditorEngine.h"		// class UNREALED_API UEditorEngine : public UEngine
+#include "MyEditorEngineBase.generated.h"
+
+// error : UCLASS inside this preprocessor block will be skipped
+UCLASS(config = Engine, transient)
+class UMyEditorEngineBase : public UEditorEngine
+{
+	// 没有这行代码，不能注册进虚拟机脚本
+	GENERATED_BODY()
+
+public:
+	UMyEditorEngineBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+};
+
+//#endif
