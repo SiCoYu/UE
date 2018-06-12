aa9898737f24c1431c20e03465c623eab049a2eb
codekuangben <kuangben2001@163.com>
2018-06-12 23:09:11 +0800

    aaa

---
+++ Project/MyProject/MyProject/Source/MyProject/Private/BasicFrame/Libs/Auxiliary/AuxBase/AuxComponent.cpp
@@ -0,0 +1,21 @@
+﻿#include "MyProject.h"
+#include "AuxComponent.h"
+
+MY_BEGIN_NAMESPACE(MyNS)
+
+AuxComponent::AuxComponent()
+{
+	
+}
+
+void AuxComponent::init()
+{
+	
+}
+
+void AuxComponent::dispose()
+{
+
+}
+
+MY_END_NAMESPACE
---
+++ Project/MyProject/MyProject/Source/MyProject/Private/BasicFrame/Libs/Auxiliary/AuxBase/AuxComponent.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include "GObject.h"
+#include "PlatformDefine.h"
+
+MY_BEGIN_NAMESPACE(MyNS)
+
+class AuxComponent : public GObject
+{
+protected:
+
+public:
+	AuxComponent();
+
+public:
+	void init();
+	void dispose();
+};
+
+MY_END_NAMESPACE
