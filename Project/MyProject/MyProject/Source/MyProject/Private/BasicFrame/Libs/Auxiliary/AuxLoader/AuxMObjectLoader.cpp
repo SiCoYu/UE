e15e6fa5077f55b21483e1b1b0c8a7145f40c3f2
codekuangben <kuangben2001@163.com>
2018-06-12 21:43:48 +0800

    aaa

--- Project/MyProject/MyProject/Source/MyProject/Private/BasicFrame/Libs/Auxiliary/AuxLoader/AuxMObjectLoader.cpp
+++ Project/MyProject/MyProject/Source/MyProject/Private/BasicFrame/Libs/Auxiliary/AuxLoader/AuxMObjectLoader.cpp
@@ -6,7 +6,11 @@ MY_BEGIN_NAMESPACE(MyNS)
 
 M_IMPLEMENT_AND_REGISTER_CLASS(AuxMObjectLoader, AuxMObjectLoaderBase)
 
-AuxMObjectLoader::AuxMObjectLoader(std::string path, bool isNeedInsPrefab, bool isInsNeedCoroutine)
+AuxMObjectLoader::AuxMObjectLoader(
+	std::string path, 
+	bool isNeedInsPrefab, 
+	bool isInsNeedCoroutine
+)
 	: Super(path, isNeedInsPrefab, isInsNeedCoroutine)
 {
 	this->mResPackType = ResPackType::eObjectType;
