327ac5b810bda5f3ca5f11ce93c6b45d2750b89d
codekuangben <kuangben2001@163.com>
2018-06-19 23:31:05 +0800

    aaa

---
+++ Project/MyProject/MyProject/Source/MyProject/Private/BasicFrame/Libs/EventHandle/PromiseEventDispatch.cpp
@@ -0,0 +1,42 @@
+﻿#include "MyProject.h"
+#include "PromiseEventDispatch.h"
+#include "IDispatchObject.h"
+#include "MClassFactory.h"
+
+MY_BEGIN_NAMESPACE(MyNS)
+
+M_IMPLEMENT_AND_REGISTER_CLASS(PromiseEventDispatch, AddOnceEventDispatch)
+
+PromiseEventDispatch::PromiseEventDispatch()
+{
+	this->mEventState = -1;
+	this->mIsValid = false;
+}
+
+void PromiseEventDispatch::setEventState(int value)
+{
+	this->mIsValid = true;
+	this->mEventState = value;
+
+	Super::dispatchEvent(nullptr);
+}
+
+void PromiseEventDispatch::clearEventState(int value)
+{
+	this->mIsValid = false;
+	this->mEventState = -1;
+}
+
+void PromiseEventDispatch::addEventHandle(
+	EventDispatchDelegate handle
+) 
+{
+	if (this->mIsValid && !handle.empty())
+	{
+		handle.call(nullptr);
+	}
+
+	Super::addEventHandle(handle);
+}
+
+MY_END_NAMESPACE
---
+++ Project/MyProject/MyProject/Source/MyProject/Private/BasicFrame/Libs/EventHandle/PromiseEventDispatch.h
@@ -0,0 +1,35 @@
+﻿#ifndef __PromiseEventDispatch_H
+#define __PromiseEventDispatch_H
+
+#include "EventDispatchDelegate.h"
+#include "AddOnceEventDispatch.h"
+#include "MClassInfo.h"
+#include "MClassMacros.h"
+#include "PlatformDefine.h"
+
+MY_BEGIN_NAMESPACE(MyNS)
+
+/**
+ * @brief 记录状态的事件分发，永远不会漏掉事件
+ */
+class PromiseEventDispatch : public AddOnceEventDispatch
+{
+	M_DECLARE_CLASS(PromiseEventDispatch, AddOnceEventDispatch)
+	
+protected:
+	int mEventState;
+	bool mIsValid;
+
+public:
+	PromiseEventDispatch();
+
+	void setEventState(int value);
+	void clearEventState(int value);
+	virtual void addEventHandle(
+		EventDispatchDelegate handle
+	) override;
+}
+	
+MY_END_NAMESPACE
+
+#endif
