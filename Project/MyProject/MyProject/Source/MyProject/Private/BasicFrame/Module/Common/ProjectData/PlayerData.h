f43b75e6c162dcfc70fd94449985dd645bf727c6
kuangben2001 <kuangben2001@163.com>
2018-06-20 15:45:51 +0800

    aaa

---
+++ Project/MyProject/MyProject/Source/MyProject/Private/BasicFrame/Module/Common/ProjectData/PlayerData.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "GObject.h"
+#include "MClassInfo.h"
+#include "MClassMacros.h"
+
+MY_BEGIN_NAMESPACE(MyNS)
+
+/**
+ * @brief 基本是主角自己的数据
+ */
+class PlayerData : public GObject
+{
+	M_DECLARE_CLASS(PlayerData, GObject)
+
+public:
+	EntityData();
+
+	void init();
+	void dispose();
+};
+
+MY_END_NAMESPACE
