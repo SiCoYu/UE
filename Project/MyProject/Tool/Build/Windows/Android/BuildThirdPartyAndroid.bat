7f9929601d8b2d0ac2ebca1db9112ef8de53cade
codekuangben <kuangben2001@163.com>
2018-06-27 00:26:17 +0800

    aaa

--- Project/MyProject/Tool/Build/Windows/Android/BuildThirdPartyAndroid.bat
+++ Project/MyProject/Tool/Build/Windows/Android/BuildThirdPartyAndroid.bat
@@ -16,6 +16,10 @@ if not defined BuildParam (
 	)
 )
 
+rem G:\ProgramFiles\cmake-3.7.2-win64-x64\bin\cmake.exe G:/UE/Project/MyProject/MyProject/ThirdParty/Src -G "Unix Makefiles" -DANDROID_NDK=G:/ProgramFiles/android/android-ndk-r17b/ -DCMAKE_TOOLCHAIN_FILE=G:/ProgramFiles/android/android-ndk-r17b/build/cmake/android.toolchain.cmake -DANDROID_ABI=arm64-v8a
+rem https://developer.android.google.cn/ndk/guides/cmake
+rem https://stackoverflow.com/questions/32753905/cmake-android-ndk-toolchain-on-windows
+rem https://blog.csdn.net/xinyu391/article/details/73614692
 %CMAKE_BIN% -DCMAKE_TOOLCHAIN_FILE=%NDK_TOOLCHAIN_FILE% -DANDROID_ABI=whatever %THIRDPARTY_SRC%
 
 rem pause
