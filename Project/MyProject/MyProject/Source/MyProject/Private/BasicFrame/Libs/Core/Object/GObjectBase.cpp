7776c804f9aa4ca7a25f1c5be6f135ab4bbb173f
kuangben2001 <kuangben2001@163.com>
2018-06-28 09:42:06 +0800

    aaa

--- Project/MyProject/MyProject/Source/MyProject/Private/BasicFrame/Libs/Core/Object/GObjectBase.cpp
+++ Project/MyProject/MyProject/Source/MyProject/Private/BasicFrame/Libs/Core/Object/GObjectBase.cpp
@@ -3,6 +3,9 @@
 #include "TypeDef.h"
 #include "MClassFactory.h"
 
+// Engine\Source\Runtime\CoreUObject\Public\UObject\UObject.h
+// warning C4996: 'UObjectHeaderDeprecatedWarning': Including UObject.h has been deprecated. Please include Object.h instead. Please update your code to the new API before upgrading to the next release, otherwise your project will no longer compile.
+
 MY_BEGIN_NAMESPACE(MyNS)
 
 M_IMPLEMENT_ROOT_CLASS(GObjectBase)
