a4ece149c88c4eb4a40a4c9dee1f4a5a74156555
codekuangben <kuangben2001@163.com>
2018-07-09 22:22:59 +0800

    aaa

---
+++ Project/MyProject/MyProject/Source/MyProject/Private/BasicFrame/Libs/Resource/AutoUpdate/FileGroup.cs
@@ -0,0 +1,269 @@
+﻿namespace SDK.Lib
+{
+    /**
+     * @brief 一组文件的统计信息
+     */
+    public class FileGroup
+    {
+        protected int mCurNum;          // 当前已经更新的资源数量
+        protected int mTotalNum;        // 总共需要更新的资源数量
+        protected int mCurMemSize;      // 当前资源内存总共大小，单位 byte
+        protected int mTotalMemSize;    // 总共资源内存总共大小，单位 byte
+
+        protected MList<string> mLoadingPath;       // 将要下载
+        protected MList<string> mLoadedPath;        // 已经下载成功
+        protected MList<string> mFailedPath;        // 下载失败
+
+        protected MDictionary<string, bool> mUpdatePathDic;    // 更新的资源目录
+
+        protected AddOnceEventDispatch mProgressDispatch;   // 进度事件分发
+
+        public FileGroup()
+        {
+            this.mCurNum = 0;
+            this.mTotalNum = 0;
+            this.mCurMemSize = 0;
+            this.mTotalMemSize = 0;
+
+            this.mLoadingPath = new MList<string>();
+            this.mLoadedPath = new MList<string>();
+            this.mFailedPath = new MList<string>();
+            this.mUpdatePathDic = new MDictionary<string, bool>();
+
+            this.mProgressDispatch = new AddOnceEventDispatch();
+
+            this.reset();
+        }
+
+        public bool isUpdateSuccess()
+        {
+            return (this.mCurNum == this.mTotalNum &&
+                   0 == this.mFailedPath.count() &&
+                   0 == this.mLoadingPath.count());
+        }
+
+        public bool isCurEqTotal()
+        {
+            return this.mCurNum == this.mTotalNum;
+        }
+
+        public void reset()
+        {
+            this.mCurNum = 0;
+            this.mTotalNum = 0;
+
+            this.mLoadingPath.clear();
+            this.mLoadedPath.clear();
+            this.mFailedPath.clear();
+        }
+
+        public void setTotalNum(int value)
+        {
+            this.mTotalNum = value;
+        }
+
+        public int getTotalNum()
+        {
+            return this.mTotalNum;
+        }
+
+        public void incCurNum()
+        {
+            this.mCurNum += 1;
+        }
+
+        public int getCurNum()
+        {
+            return this.mCurNum;
+        }
+
+        // 有正在加载的内容
+        public bool hasLoading()
+        {
+            return this.mLoadingPath.count() > 0;
+        }
+
+        // 有加载失败的
+        public bool hasLoadFailed()
+        {
+            return this.mFailedPath.count() > 0;
+        }
+
+        // 增加更新的文件
+        public void addLoadingPath(string path, FileVerInfo fileVerInfo /*, bool isAddTotalNum = false*/)
+        {
+            this.mTotalMemSize += fileVerInfo.mFileSize;
+
+            this.mLoadingPath.add(path);
+            this.mUpdatePathDic.add(path, true);    // 开始加载设置加载标志
+
+            //if(isAddTotalNum)
+            //{
+            //    this.mTotalNum += 1;
+            //}
+
+            //this.mProgressDispatch.dispatchEvent(null);
+        }
+
+        public void removeLoadingPath(string path/*, bool isDecTotalNum = false*/)
+        {
+            FileVerInfo fileVerInfo = Ctx.msInstance.mVersionSys.mServerVer.mABPath2HashDic[path];
+
+            if (null != fileVerInfo)
+            {
+                this.mCurMemSize += fileVerInfo.mFileSize;
+            }
+
+            this.mLoadingPath.remove(path);
+
+            //if (isDecTotalNum)
+            //{
+            //    this.mTotalNum -= 1;
+            //}
+
+            this.mProgressDispatch.dispatchEvent(null);
+        }
+
+        public void addLoadedPath(string path/*, bool isAddTotalNum = false*/)
+        {
+            this.mLoadedPath.add(path);
+
+            //if (isAddTotalNum)
+            //{
+            //    this.mTotalNum += 1;
+            //}
+        }
+
+        public void addFailedPath(string path/*, bool isAddTotalNum = false*/)
+        {
+            this.mFailedPath.add(path);
+            this.mUpdatePathDic.remove(path);   // 如果加载失败，就是没有更新的资源，需要移除这个更新的资源
+
+            //if (isAddTotalNum)
+            //{
+            //    this.mTotalNum += 1;
+            //}
+
+            //this.mProgressDispatch.dispatchEvent(null);
+        }
+
+        // 获取进度 [0, 100]
+        public int getProgress()
+        {
+            int curNum = this.mLoadedPath.count() + this.mFailedPath.count();
+            int totalNum = this.mLoadingPath.count() + this.mLoadedPath.count() + this.mFailedPath.count();
+
+            int progress = 0;
+
+            if (totalNum != 0)
+            {
+                progress = (int)(((float)curNum / totalNum) * 100);
+            }
+
+            return progress;
+        }
+
+        // 获取文件加载内存大小
+        public int getAllNeedUpdateMenSize()
+        {
+            return this.mTotalMemSize;
+        }
+
+        // 获取当前加载的文件大小
+        public int getCurUpdatedMenSize()
+        {
+            return this.mCurMemSize;
+        }
+
+        // 获取加载进度描述
+        public string getProgressDesc()
+        {
+            string ret = "";
+
+            ret = string.Format("{0}M/{1}M", UtilMath.integerWithFract((float)this.mCurMemSize / UtilMath.OneM, 2), UtilMath.integerWithFract((float)this.mTotalMemSize / UtilMath.OneM, 2));
+
+            return ret;
+        }
+
+        // 输出下载详细日志
+        public void logDownloadDetailInfo()
+        {
+            Ctx.msInstance.mLogSys.log(string.Format("FileGroup::logDownloadDetailInfo, mCurNum = {0}, mTotalNum = {1}", this.mCurNum, this.mTotalNum), LogTypeId.eLogAutoUpdate);
+
+            int index = 0;
+            int listLen = this.mLoadingPath.count();
+
+            Ctx.msInstance.mLogSys.log("FileGroup::logDownloadDetailInfo, ********** LoadingPath start **********", LogTypeId.eLogAutoUpdate);
+
+            while (index < listLen)
+            {
+                Ctx.msInstance.mLogSys.log(string.Format("FileGroup::logDownloadDetailInfo, index = {0}, LoadingPath = {1}", index, this.mLoadingPath[index]), LogTypeId.eLogAutoUpdate);
+
+                index += 1;
+            }
+
+            Ctx.msInstance.mLogSys.log("FileGroup::logDownloadDetailInfo, ********** LoadingPath end **********", LogTypeId.eLogAutoUpdate);
+
+            index = 0;
+            listLen = this.mLoadedPath.count();
+
+            Ctx.msInstance.mLogSys.log("FileGroup::logDownloadDetailInfo, ********** mLoadedPath start **********", LogTypeId.eLogAutoUpdate);
+
+            while (index < listLen)
+            {
+                Ctx.msInstance.mLogSys.log(string.Format("FileGroup::logDownloadDetailInfo, index = {0}, mLoadedPath = {1}", index, this.mLoadedPath[index]), LogTypeId.eLogAutoUpdate);
+
+                index += 1;
+            }
+
+            Ctx.msInstance.mLogSys.log("FileGroup::logDownloadDetailInfo, ********** mLoadedPath end **********", LogTypeId.eLogAutoUpdate);
+
+            index = 0;
+            listLen = this.mFailedPath.count();
+
+            Ctx.msInstance.mLogSys.log("FileGroup::logDownloadDetailInfo, ********** mFailedPath start **********", LogTypeId.eLogAutoUpdate);
+
+            while (index < listLen)
+            {
+                Ctx.msInstance.mLogSys.log(string.Format("FileGroup::logDownloadDetailInfo, index = {0}, mFailedPath = {1}", index, this.mFailedPath[index]), LogTypeId.eLogAutoUpdate);
+
+                index += 1;
+            }
+
+            Ctx.msInstance.mLogSys.log("FileGroup::logDownloadDetailInfo, ********** mFailedPath end **********", LogTypeId.eLogAutoUpdate);
+        }
+
+        // 获取当前资源是否更新了
+        public bool isResUpdatedByResPath(string path)
+        {
+            bool ret = false;
+
+            ResRedirectItem resRedirectItem = Ctx.msInstance.mResRedirect.getResRedirectItem(ShaderCollectPath.StartShaderCollect, false);
+
+            if (null != resRedirectItem)
+            {
+                ret = this.isResUpdatedByABPath(resRedirectItem.mFileVerInfo.mLoadPath);
+            }
+
+            return ret;
+        }
+
+        // 获取当前资源是否更新了
+        public bool isResUpdatedByABPath(string path)
+        {
+            bool ret = false;
+            this.mUpdatePathDic.tryGetValue(path, out ret);
+            return ret;
+        }
+
+        public void addProgressHandle(ICalleeObject pThis, MEventDispatchAction<IDispatchObject> handle)
+        {
+            this.mProgressDispatch.addEventHandle(pThis, handle);
+        }
+
+        public void removeProgressHandle(ICalleeObject pThis, MEventDispatchAction<IDispatchObject> handle)
+        {
+            this.mProgressDispatch.removeEventHandle(pThis, handle);
+        }
+    }
+}
---
+++ Project/MyProject/MyProject/Source/MyProject/Private/BasicFrame/Libs/Resource/Version/VersionSys.cs
@@ -0,0 +1,321 @@
+﻿namespace SDK.Lib
+{
+    /**
+     * @brief 版本系统，文件格式   path=value
+     */
+    public class VersionSys
+    {
+        public ServerVer mServerVer;
+        public LocalVer mLocalVer;
+
+        public AddOnceAndCallOnceEventDispatch mMiniLoadResultDispatch;
+        public AddOnceAndCallOnceEventDispatch mLoadResultDispatch;
+
+        public bool mIsNeedUpdateApp;       // 是否需要更新 App
+        public bool mIsNeedUpdateVerFile;   // 是否需要更新版本文件
+
+        public string mMiniVer;    // mini 版本文件版本号
+
+        public VersionSys()
+        {
+            this.mMiniVer = UtilEngineWrap.Range(0, int.MaxValue).ToString();
+            this.mMiniLoadResultDispatch = new AddOnceAndCallOnceEventDispatch();
+            this.mLoadResultDispatch = new AddOnceAndCallOnceEventDispatch();
+
+            this.mServerVer = new ServerVer();
+            this.mLocalVer = new LocalVer();
+        }
+
+        public void init()
+        {
+            
+        }
+
+        public void loadAllLocalVer()
+        {
+            this.mLocalVer.loadAllLocalVer();
+        }
+
+        public string getMajorVersion()
+        {
+            string ret = "";
+
+            if(this.mServerVer.mIsMiniLoadSuccess)
+            {
+                ret = this.mServerVer.mMajorVersion;
+            }
+            else
+            {
+                ret = this.mLocalVer.mMajorVersion;
+            }
+
+            return ret;
+        }
+
+        public string getMinorVersion()
+        {
+            string ret = "";
+
+            if (this.mServerVer.mIsMiniLoadSuccess)
+            {
+                ret = this.mServerVer.mMinorVersion;
+            }
+            else
+            {
+                ret = this.mLocalVer.mMinorVersion;
+            }
+
+            return ret;
+        }
+
+        public string getPatchVersion()
+        {
+            string ret = "";
+
+            if (this.mServerVer.mIsMiniLoadSuccess)
+            {
+                ret = this.mServerVer.mPatchVersion;
+            }
+            else
+            {
+                ret = this.mLocalVer.mPatchVersion;
+            }
+
+            return ret;
+        }
+
+        public string getTimeStamp()
+        {
+            string ret = "";
+
+            if (this.mServerVer.mIsMiniLoadSuccess)
+            {
+                ret = this.mServerVer.mTimeStamp;
+            }
+            else
+            {
+                ret = this.mLocalVer.mTimeStamp;
+            }
+
+            return ret;
+        }
+
+        public string getDetailVersionString()
+        {
+            string ret = "";
+
+            if (this.mServerVer.mIsMiniLoadSuccess)
+            {
+                ret = this.mServerVer.getDetailVersionString();
+            }
+            else
+            {
+                ret = this.mLocalVer.getDetailVersionString();
+            }
+
+            return ret;
+        }
+
+        public string getDotVersionString()
+        {
+            string ret = "";
+
+            if (this.mServerVer.mIsMiniLoadSuccess)
+            {
+                ret = this.mServerVer.getDotVersionString();
+            }
+            else
+            {
+                ret = this.mLocalVer.getDotVersionString();
+            }
+
+            return ret;
+        }
+
+        public string getVersionString()
+        {
+            string ret = "";
+
+            if (this.mServerVer.mIsMiniLoadSuccess)
+            {
+                ret = this.mServerVer.getVersionString();
+            }
+            else
+            {
+                ret = this.mLocalVer.getVersionString();
+            }
+
+            return ret;
+        }
+
+        public void loadWebMiniVerFile()
+        {
+            if (MacroDef.ENABLE_LOG)
+            {
+                Ctx.msInstance.mLogSys.log("VersionSys::loadWebMiniVerFile", LogTypeId.eLogAutoUpdate);
+            }
+
+            this.mServerVer.mMiniLoadedDispatch.addEventHandle(null, this.onWebMiniLoaded);
+            this.mServerVer.loadMiniVerFile();
+        }
+
+        public void loadWebVerFile()
+        {
+            if (MacroDef.ENABLE_LOG)
+            {
+                Ctx.msInstance.mLogSys.log("VersionSys::loadWebVerFile, start", LogTypeId.eLogAutoUpdate);
+            }
+
+            if (this.mLocalVer.mIsVerLoadSuccess)
+            {
+                if (MacroDef.ENABLE_LOG)
+                {
+                    Ctx.msInstance.mLogSys.log("VersionSys::loadWebVerFile, mLocalVer.mIsVerLoadSuccess is true", LogTypeId.eLogAutoUpdate);
+                }
+
+                if (this.mIsNeedUpdateVerFile)
+                {
+                    if (MacroDef.ENABLE_LOG)
+                    {
+                        Ctx.msInstance.mLogSys.log("VersionSys::loadWebVerFile, mIsNeedUpdateVerFile is true", LogTypeId.eLogAutoUpdate);
+                    }
+
+                    this.mServerVer.mLoadedDispatch.addEventHandle(null, this.onWebVerLoaded);
+                    this.mServerVer.loadVerFile();
+                }
+                else
+                {
+                    if (MacroDef.ENABLE_LOG)
+                    {
+                        Ctx.msInstance.mLogSys.log("VersionSys::loadWebVerFile, mIsNeedUpdateVerFile is false", LogTypeId.eLogAutoUpdate);
+                    }
+
+                    this.mLoadResultDispatch.dispatchEvent(null);
+                }
+            }
+            else
+            {
+                if (MacroDef.ENABLE_LOG)
+                {
+                    Ctx.msInstance.mLogSys.log("VersionSys::loadWebVerFile, mLocalVer.mIsVerLoadSuccess is false", LogTypeId.eLogAutoUpdate);
+                }
+
+                if (this.mIsNeedUpdateVerFile)
+                {
+                    if (MacroDef.ENABLE_LOG)
+                    {
+                        Ctx.msInstance.mLogSys.log("VersionSys::loadWebVerFile, mIsNeedUpdateVerFile is true", LogTypeId.eLogAutoUpdate);
+                    }
+
+                    this.mServerVer.mLoadedDispatch.addEventHandle(null, this.onWebVerLoaded);
+                    this.mServerVer.loadVerFile();
+                }
+                else
+                {
+                    if (MacroDef.ENABLE_LOG)
+                    {
+                        Ctx.msInstance.mLogSys.log("VersionSys::loadWebVerFile, mIsNeedUpdateVerFile is false", LogTypeId.eLogAutoUpdate);
+                    }
+
+                    this.mLoadResultDispatch.dispatchEvent(null);
+                }
+            }
+        }
+
+        public void onWebMiniLoaded(IDispatchObject dispObj, uint uniqueId)
+        {
+            if (this.mServerVer.mIsMiniLoadSuccess)
+            {
+                // 删除旧 mini 版本，修改新版本文件名字
+                //UtilFileIO.deleteFile(Path.Combine(MFileSys.getLocalWriteDir(), VerFileName.VER_P));
+                // 修改新的版本文件名字
+                //UtilFileIO.renameFile(UtilLogic.combineVerPath(Path.Combine(MFileSys.getLocalWriteDir(), VerFileName.VER_MINI), m_miniVer), Path.Combine(MFileSys.getLocalWriteDir(), VerFileName.VER_MINI));
+
+                this.mIsNeedUpdateApp = (this.mLocalVer.mMajorVersion != this.mServerVer.mMajorVersion);      // 如果版本不一致，需要重新加载 App
+                this.mIsNeedUpdateVerFile = (this.mLocalVer.mFileVerInfo.mFileMd5 != this.mServerVer.mFileVerInfo.mFileMd5);      // 如果版本不一致，需要重新加载版本文件
+
+                //this.mIsNeedUpdateVerFile = true;         // 测试强制更新
+
+                if (MacroDef.ENABLE_LOG)
+                {
+                    Ctx.msInstance.mLogSys.log(string.Format("VersionSys::onWebMiniLoaded, loaded, mIsNeedUpdateApp = {0}, mIsNeedUpdateVerFile = {1}", this.mIsNeedUpdateApp.ToString(), this.mIsNeedUpdateVerFile.ToString()), LogTypeId.eLogAutoUpdate);
+                }
+            }
+            else
+            {
+                if (MacroDef.ENABLE_LOG)
+                {
+                    Ctx.msInstance.mLogSys.log("VersionSys::onWebMiniLoaded, failed", LogTypeId.eLogAutoUpdate);
+                }
+
+                // 如果没有加载到服务器版本文件，就不用更新了
+                this.mIsNeedUpdateApp = false;
+                this.mIsNeedUpdateVerFile = false;
+            }
+
+            this.mMiniLoadResultDispatch.dispatchEvent(null);
+        }
+
+        public void onWebVerLoaded(IDispatchObject dispObj, uint uniqueId)
+        {
+            if (MacroDef.ENABLE_LOG)
+            {
+                Ctx.msInstance.mLogSys.log("VersionSys::onWebVerLoaded", LogTypeId.eLogAutoUpdate);
+            }
+
+            this.mLoadResultDispatch.dispatchEvent(null);
+        }
+
+        public string getFileVer(string path)
+        {
+            if(this.mIsNeedUpdateVerFile)
+            {
+                if (this.mServerVer.mPath2HashDic.containsKey(path))
+                {
+                    return this.mServerVer.mPath2HashDic[path].mFileMd5;
+                }
+            }
+            else
+            {
+                if (this.mLocalVer.mPath2Ver_P_Dic.containsKey(path))
+                {
+                    return this.mLocalVer.mPath2Ver_P_Dic[path].mFileMd5;
+                }
+            }
+
+            return "";
+        }
+
+        // 保存 VerFileName.VER_MINI 版本文件和 VerFileName.VER_P 版本文件到 Persistent 文件夹
+        public void saveWebMiniOrPVerToPersistentPath()
+        {
+            if (MacroDef.ENABLE_LOG)
+            {
+                Ctx.msInstance.mLogSys.log("VersionSys::saveWebMiniOrPVerToPersistentPath", LogTypeId.eLogAutoUpdate);
+            }
+
+            this.mServerVer.saveMiniVerToPersistentPath();
+
+            if(this.mIsNeedUpdateVerFile)
+            {
+                this.mServerVer.savePVerToPersistentPath();
+            }
+        }
+
+        // 如果 WebVerFile 版本文件更细了，就更新本地版本文件
+        public void updateLocalVerFile()
+        {
+            if(null != this.mLocalVer.mPath2Ver_P_Dic)
+            {
+                this.mLocalVer.mPath2Ver_P_Dic.clear();
+                this.mLocalVer.mABPath2Ver_P_Dic.clear();
+            }
+
+            this.mLocalVer.mPath2Ver_P_Dic = this.mServerVer.mPath2HashDic;
+            this.mLocalVer.mABPath2Ver_P_Dic = this.mServerVer.mABPath2HashDic;
+
+            this.mServerVer.mPath2HashDic = null;
+            this.mServerVer.mABPath2HashDic = null;
+        }
+    }
+}
