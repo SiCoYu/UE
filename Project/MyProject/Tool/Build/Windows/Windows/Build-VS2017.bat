e35718a7189bcfaa5fba8d2882df9d19c25fbb2d
kuangben2001 <kuangben2001@163.com>
2018-07-19 18:48:20 +0800

    aaa

---
+++ Project/MyProject/Tool/Build/Windows/Windows/Build-VS2017.bat
@@ -0,0 +1,15 @@
+::echo off
+echo on
+
+set BASE_DIR=%~dp0
+echo "BASE_DIR="%BASE_DIR%
+
+set DRIVER=%BASE_DIR:~0,2%
+echo "DRIVER="%DRIVER%
+
+%DRIVER%
+cd %BASE_DIR%
+
+.\GenerateProjectFiles.bat -2017
+
+rem pause
