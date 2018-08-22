5fd29995705659ea43dd91680791ae62784d9c26
kuangben2001 <kuangben2001@163.com>
2018-08-22 16:12:38 +0800

    修改

--- Test/TestCMakeAndroid/Build.bat
+++ Test/TestCMakeAndroid/Build.bat
@@ -7,6 +7,6 @@ echo "DRIVER="%DRIVER%
 %DRIVER%
 cd %BASE_DIR%
 
-D:\ProgramFiles\cmake-3.11.3-win64-x64\bin\cmake -DDEBUG=ON -DCMAKE_TOOLCHAIN_FILE=C:/ProgramFiles/Android/android-ndk-r17b/build/cmake/android.toolchain.cmake -DANDROID_NDK=C:/ProgramFiles/Android/android-ndk-r17b -DANDROID_ABI=armeabi-v7a -DANDROID_TOOLCHAIN=clang -DANDROID_PLATFORM=android-21 -DANDROID_STL=c++_shared
+D:\ProgramFiles\cmake-3.11.3-win64-x64\bin\cmake -DDEBUG=ON -DCMAKE_TOOLCHAIN_FILE=C:/ProgramFiles/Android/android-ndk-r15c/build/cmake/android.toolchain.cmake -DANDROID_NDK=C:/ProgramFiles/Android/android-ndk-r15c -DANDROID_ABI=armeabi-v7a -DANDROID_TOOLCHAIN=clang -DANDROID_PLATFORM=android-21 -DANDROID_STL=c++_shared
 
 pause
