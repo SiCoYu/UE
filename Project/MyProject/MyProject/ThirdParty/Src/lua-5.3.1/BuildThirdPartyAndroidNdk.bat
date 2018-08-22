c9773b39c116863ea1d89b99da8f7795596b1035
codekuangben <kuangben2001@163.com>
2018-08-22 23:12:25 +0800

    aaa

--- Project/MyProject/MyProject/ThirdParty/Src/lua-5.3.1/BuildThirdPartyAndroidNdk.bat
+++ Project/MyProject/MyProject/ThirdParty/Src/lua-5.3.1/BuildThirdPartyAndroidNdk.bat
@@ -12,4 +12,5 @@ cd %BASE_DIR%
 
 rem https://blog.csdn.net/u012702547/article/details/48196933
 rem G:\ProgramFiles\android\android-ndk-r17b\ndk-build.cmd -B V=1 NDK_LOG=1 -j 2 -C %BASE_DIR% NDK_PROJECT_PATH=. APP_BUILD_SCRIPT=./Android.mk NDK_APPLICATION_MK=./Application.mk
-G:\ProgramFiles\android\android-ndk-r17b\ndk-build.cmd -B NDK_PROJECT_PATH=. APP_BUILD_SCRIPT=./Android.mk NDK_APPLICATION_MK=./Application.mk APP_PLATFORM=android-19 APP_ABI=armeabi-v7a MY_DEBUG=DEBUG
+rem APP_PLATFORM 一定要和 Application.mk 中定义一致
+G:\ProgramFiles\android\android-ndk-r17b\ndk-build.cmd -B NDK_PROJECT_PATH=. APP_BUILD_SCRIPT=./Android.mk NDK_APPLICATION_MK=./Application.mk APP_PLATFORM=android-22 APP_ABI=armeabi-v7a MY_DEBUG=DEBUG
