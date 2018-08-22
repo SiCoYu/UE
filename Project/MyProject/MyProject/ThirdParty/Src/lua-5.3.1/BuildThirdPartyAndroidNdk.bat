1e5139d0ddbe51cc51b7900e022a3713a341f74e
codekuangben <kuangben2001@163.com>
2018-08-22 22:56:41 +0800

    aaa

--- Project/MyProject/MyProject/ThirdParty/Src/lua-5.3.1/BuildThirdPartyAndroidNdk.bat
+++ Project/MyProject/MyProject/ThirdParty/Src/lua-5.3.1/BuildThirdPartyAndroidNdk.bat
@@ -10,4 +10,6 @@ echo "DRIVER="%DRIVER%
 %DRIVER%
 cd %BASE_DIR%
 
-G:\ProgramFiles\android\android-ndk-r17b\ndk-build.cmd -B -v NDK_LOG=1 -j 2 -C %BASE_DIR% NDK_PROJECT_PATH=. APP_BUILD_SCRIPT=./Android.mk NDK_APPLICATION_MK=./Application.mk
+rem https://blog.csdn.net/u012702547/article/details/48196933
+rem G:\ProgramFiles\android\android-ndk-r17b\ndk-build.cmd -B V=1 NDK_LOG=1 -j 2 -C %BASE_DIR% NDK_PROJECT_PATH=. APP_BUILD_SCRIPT=./Android.mk NDK_APPLICATION_MK=./Application.mk
+G:\ProgramFiles\android\android-ndk-r17b\ndk-build.cmd -B NDK_PROJECT_PATH=. APP_BUILD_SCRIPT=./Android.mk NDK_APPLICATION_MK=./Application.mk
