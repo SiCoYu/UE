21fa2d922945f74e78532322f1e2d3e0a0a222de
kuangben2001 <kuangben2001@163.com>
2018-08-22 16:35:10 +0800

    添加 android 编译

---
+++ Project/MyProject/MyProject/ThirdParty/Src/luasocket-master/Android.mk
@@ -0,0 +1,88 @@
+#http://aigo.iteye.com/blog/2288694
+#http://www.cnblogs.com/hesiming/archive/2011/03/15/1984444.html
+LOCAL_PATH := $(call my-dir)
+
+###########################
+#
+# shared library
+#
+###########################
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := Lua
+
+LOCAL_MODULE_FILENAME := libLua
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/src
+
+#LOCAL_EXPORT_C_INCLUDES := $(LOCAL_C_INCLUDES)
+
+LOCAL_SRC_FILES := \
+	$(LOCAL_PATH)/src/lapi.c \
+	$(LOCAL_PATH)/src/lapi.h \
+	$(LOCAL_PATH)/src/lauxlib.c \
+	$(LOCAL_PATH)/src/lauxlib.h \
+	$(LOCAL_PATH)/src/lbaselib.c \
+	$(LOCAL_PATH)/src/lbitlib.c \
+	$(LOCAL_PATH)/src/lcode.c \
+	$(LOCAL_PATH)/src/lcode.h \
+	$(LOCAL_PATH)/src/lcorolib.c \
+	$(LOCAL_PATH)/src/lctype.c \
+	$(LOCAL_PATH)/src/lctype.h \
+	$(LOCAL_PATH)/src/ldblib.c \
+	$(LOCAL_PATH)/src/ldebug.c \
+	$(LOCAL_PATH)/src/ldebug.h \
+	$(LOCAL_PATH)/src/ldo.c \
+	$(LOCAL_PATH)/src/ldo.h \
+	$(LOCAL_PATH)/src/ldump.c \
+	$(LOCAL_PATH)/src/lfunc.c \
+	$(LOCAL_PATH)/src/lfunc.h \
+	$(LOCAL_PATH)/src/lgc.c \
+	$(LOCAL_PATH)/src/lgc.h \
+	$(LOCAL_PATH)/src/linit.c \
+	$(LOCAL_PATH)/src/liolib.c \
+	$(LOCAL_PATH)/src/llex.c \
+	$(LOCAL_PATH)/src/llex.h \
+	$(LOCAL_PATH)/src/llimits.h \
+	$(LOCAL_PATH)/src/lmathlib.c \
+	$(LOCAL_PATH)/src/lmem.c \
+	$(LOCAL_PATH)/src/lmem.h \
+	$(LOCAL_PATH)/src/loadlib.c \
+	$(LOCAL_PATH)/src/lobject.c \
+	$(LOCAL_PATH)/src/lobject.h \
+	$(LOCAL_PATH)/src/lopcodes.c \
+	$(LOCAL_PATH)/src/lopcodes.h \
+	$(LOCAL_PATH)/src/loslib.c \
+	$(LOCAL_PATH)/src/lparser.c \
+	$(LOCAL_PATH)/src/lparser.h \
+	$(LOCAL_PATH)/src/lprefix.h \
+	$(LOCAL_PATH)/src/lstate.c \
+	$(LOCAL_PATH)/src/lstate.h \
+	$(LOCAL_PATH)/src/lstring.c \
+	$(LOCAL_PATH)/src/lstring.h \
+	$(LOCAL_PATH)/src/lstrlib.c \
+	$(LOCAL_PATH)/src/ltable.c \
+	$(LOCAL_PATH)/src/ltable.h \
+	$(LOCAL_PATH)/src/ltablib.c \
+	$(LOCAL_PATH)/src/ltm.c \
+	$(LOCAL_PATH)/src/ltm.h \
+	$(LOCAL_PATH)/src/lua.c \
+	$(LOCAL_PATH)/src/lua.h \
+	$(LOCAL_PATH)/src/lua.hpp \
+	$(LOCAL_PATH)/src/luaconf.h \
+	$(LOCAL_PATH)/src/lualib.h \
+	$(LOCAL_PATH)/src/lundump.c \
+	$(LOCAL_PATH)/src/lundump.h \
+	$(LOCAL_PATH)/src/lutf8lib.c \
+	$(LOCAL_PATH)/src/lvm.c \
+	$(LOCAL_PATH)/src/lvm.h \
+	$(LOCAL_PATH)/src/lzio.c \
+	$(LOCAL_PATH)/src/lzio.h
+
+LOCAL_CFLAGS += -DTEST_DEF -fPIC
+#LOCAL_CXXFLAGS += -DGL_GLEXT_PROTOTYPES
+#LOCAL_CPPFLAGS += -DGL_GLEXT_PROTOTYPES
+#LOCAL_LDLIBS := -ldl -lGLESv1_CM -lGLESv2 -llog -landroid
+
+include $(BUILD_SHARED_LIBRARY)
---
+++ Project/MyProject/MyProject/ThirdParty/Src/luasocket-master/BuildThirdPartyAndroidNdk.bat
@@ -0,0 +1,13 @@
+::echo off
+echo on
+
+set BASE_DIR=%~dp0
+echo "BASE_DIR="%BASE_DIR%
+
+set DRIVER=%BASE_DIR:~0,2%
+echo "DRIVER="%DRIVER%
+
+%DRIVER%
+cd %BASE_DIR%
+
+C:\ProgramFiles\Android\android-ndk-r17b\ndk-build.cmd NDK_PROJECT_PATH=. APP_BUILD_SCRIPT=./Android.mk NDK_APPLICATION_MK=./Application.mk
