b7d6419ec190ebf6d1d0b246e5169be1f34b930d
codekuangben <kuangben2001@163.com>
2018-08-24 20:44:36 +0800

    aaa

--- Project/MyProject/MyProject/Plugins/MyScriptPlugin/Source/ScriptGeneratorPlugin/ScriptGeneratorPlugin.Build.cs
+++ Project/MyProject/MyProject/Plugins/MyScriptPlugin/Source/ScriptGeneratorPlugin/ScriptGeneratorPlugin.Build.cs
@@ -72,62 +72,125 @@ namespace UnrealBuildTool.Rules
             Log.TraceVerbose("ScriptGenerator LUA Integration enabled");
             PublicDefinitions.Add("WITH_LUA=1");
 
-            loadThirdPartyInclude();
-            LoadLua(Target);
+            this.loadThirdPartyInc();
+            //LoadLua(Target);
+            this.loadThirdLib(Target, "Lua");
 
             // UE 4.20 warning C4005: 'HACK_HEADER_GENERATOR': macro redefinition
             PublicDefinitions.Add("HACK_HEADER_GENERATOR=1");
         }
 
-        protected string ModulePath
+        protected string getMyModulePath()
         {
-            get
-            {
-                // “UnrealBuildTool.RulesCompiler.GetModuleFilename(string)”已过时:“GetModuleFilename is deprecated, use the ModuleDirectory property on any ModuleRules instead to get a path to your module.”
-                //return Path.GetDirectoryName(RulesCompiler.GetModuleFilename(this.GetType().Name));
-                //return Path.GetDirectoryName(ModuleDirectory);    // 不是这样获取
-                return ModuleDirectory;      // 参照 Engine\Plugins\Runtime\Nvidia\Ansel\Source\Ansel\Ansel.Build.cs 实现
-            }
+            // “UnrealBuildTool.RulesCompiler.GetModuleFilename(string)”已过时:“GetModuleFilename is deprecated, use the ModuleDirectory property on any ModuleRules instead to get a path to your module.”
+            //return Path.GetDirectoryName(RulesCompiler.GetModuleFilename(this.GetType().Name));
+            //return Path.GetDirectoryName(ModuleDirectory);    // 不是这样获取
+            return ModuleDirectory;      // 参照 Engine\Plugins\Runtime\Nvidia\Ansel\Source\Ansel\Ansel.Build.cs 实现
         }
 
-        protected string ThirdPartyPath
+        protected string getMyProjectThirdPartyPath()
         {
-            get
-            {
-                return Path.GetFullPath(Path.Combine(ModulePath, "../../../..", "ThirdParty"));
-            }
+            string ret = "";
+            ret = Path.GetFullPath(Path.Combine(this.getMyModulePath(), "../../../..", "ThirdParty"));
+            return ret;
         }
 
-        private void loadThirdPartyInclude()
+        private void loadThirdPartyInc()
         {
             PublicIncludePaths.AddRange(
                 new string[]
                 {
                     // ... add public include paths required here ...
-                    Path.Combine(ThirdPartyPath, "Inc", "Lua"),
+                    Path.Combine(this.getMyProjectThirdPartyPath(), "Inc", "Lua"),
                 }
             );
         }
 
-        private bool LoadLua(ReadOnlyTargetRules Target)
+        //private bool LoadLua(ReadOnlyTargetRules Target)
+        //{
+        //    if ((Target.Platform == UnrealTargetPlatform.Win64) || (Target.Platform == UnrealTargetPlatform.Win32))
+        //    {
+        //        string LibrariesPath;
+        //        LibrariesPath = Path.Combine(this.getMyProjectThirdPartyPath(), "Lib", "Lua", "Lua_d.lib");
+        //        PublicAdditionalLibraries.Add(LibrariesPath);
+        //    }
+        //    else if (Target.Platform == UnrealTargetPlatform.Android)
+        //    {
+        //        // UE4.19.1 error CS0122: “UnrealBuildTool.BuildConfiguration”不可访问，因为它受保护级别限制
+        //        //string BuildPath = Utils.MakePathRelativeTo(ModuleDirectory, BuildConfiguration.RelativeEnginePath);
+        //        string BuildPath = Utils.MakePathRelativeTo(ModuleDirectory, Target.RelativeEnginePath);
+
+        //        // UE4.19.1 warning CS0618: “UnrealBuildTool.ModuleRules.ReceiptPropertyList.Add(UnrealBuildTool.ReceiptProperty)”已过时:“Constructing a ReceiptProperty object is deprecated. Call RuntimeDependencies.Add() with the path to the file to stage.”
+        //        //AdditionalPropertiesForReceipt.Add(new ReceiptProperty("AndroidPlugin", Path.Combine(BuildPath, "My_APL_armv7.xml")));
+        //        RuntimeDependencies.Add(Path.Combine(BuildPath, "My_APL_armv7.xml"));
+
+        //        PublicAdditionalLibraries.Add(BuildPath + "/armv7/libLua.so");
+        //    }
+
+        //    return true;
+        //}
+
+        private bool loadThirdLib(ReadOnlyTargetRules Target, string libName)
         {
-            if ((Target.Platform == UnrealTargetPlatform.Win64) || (Target.Platform == UnrealTargetPlatform.Win32))
+            string librariesPath = "";
+
+            if (UnrealTargetPlatform.Mac == Target.Platform)
             {
-                string LibrariesPath;
-                LibrariesPath = Path.Combine(ThirdPartyPath, "Lib", "Lua", "Lua_d.lib");
-                PublicAdditionalLibraries.Add(LibrariesPath);
+
             }
-            else if (Target.Platform == UnrealTargetPlatform.Android)
+            else if (UnrealTargetPlatform.Win32 == Target.Platform)
             {
-                // UE4.19.1 error CS0122: “UnrealBuildTool.BuildConfiguration”不可访问，因为它受保护级别限制
-                //string BuildPath = Utils.MakePathRelativeTo(ModuleDirectory, BuildConfiguration.RelativeEnginePath);
-                string BuildPath = Utils.MakePathRelativeTo(ModuleDirectory, Target.RelativeEnginePath);
+                if (UnrealTargetConfiguration.Debug == Target.Configuration)
+                {
+                    librariesPath = Path.Combine(this.getMyProjectThirdPartyPath(), "Lib", libName, "Win32", libName + "_d.lib");
+                    PublicAdditionalLibraries.Add(librariesPath);
+                }
+                else
+                {
+                    librariesPath = Path.Combine(this.getMyProjectThirdPartyPath(), "Lib", libName, "Win32", libName + ".lib");
+                    PublicAdditionalLibraries.Add(librariesPath);
+                }
+            }
+            else if (UnrealTargetPlatform.Win64 == Target.Platform)
+            {
+                if (UnrealTargetConfiguration.Debug == Target.Configuration)
+                {
+                    librariesPath = Path.Combine(this.getMyProjectThirdPartyPath(), "Lib", libName, "Win64", libName + "_d.lib");
+                    PublicAdditionalLibraries.Add(librariesPath);
+                }
+                else
+                {
+                    librariesPath = Path.Combine(this.getMyProjectThirdPartyPath(), "Lib", libName, "Win64", libName + ".lib");
+                    PublicAdditionalLibraries.Add(librariesPath);
+                }
+            }
+            else if (UnrealTargetPlatform.Android == Target.Platform)
+            {
+                librariesPath = Path.Combine(this.getMyProjectThirdPartyPath(), "Lib", libName, "Android/armv7");
+                PublicLibraryPaths.Add(librariesPath);
+                librariesPath = Path.Combine(this.getMyProjectThirdPartyPath(), "Lib", libName, "Android/arm64");
+                PublicLibraryPaths.Add(librariesPath);
+                librariesPath = Path.Combine(this.getMyProjectThirdPartyPath(), "Lib", libName, "Android/x86");
+                PublicLibraryPaths.Add(librariesPath);
+                librariesPath = Path.Combine(this.getMyProjectThirdPartyPath(), "Lib", libName, "Android/x86_64");
+                PublicLibraryPaths.Add(librariesPath);
 
-                // UE4.19.1 warning CS0618: “UnrealBuildTool.ModuleRules.ReceiptPropertyList.Add(UnrealBuildTool.ReceiptProperty)”已过时:“Constructing a ReceiptProperty object is deprecated. Call RuntimeDependencies.Add() with the path to the file to stage.”
-                //AdditionalPropertiesForReceipt.Add(new ReceiptProperty("AndroidPlugin", Path.Combine(BuildPath, "My_APL_armv7.xml")));
-                RuntimeDependencies.Add(Path.Combine(BuildPath, "My_APL_armv7.xml"));
+                if (UnrealTargetConfiguration.Debug == Target.Configuration)
+                {
+                    PublicAdditionalLibraries.Add(libName + "_d");
+                }
+                else
+                {
+                    PublicAdditionalLibraries.Add(libName);
+                }
+            }
+            else if (UnrealTargetPlatform.IOS == Target.Platform)
+            {
 
-                PublicAdditionalLibraries.Add(BuildPath + "/armv7/libLua.so");
+            }
+            else
+            {
+                throw new System.Exception("Not support platform");
             }
 
             return true;
