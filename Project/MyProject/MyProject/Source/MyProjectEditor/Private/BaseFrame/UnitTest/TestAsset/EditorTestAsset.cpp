3848e374733e59c957f2bc2502bf8d5b7776f737
guanghai.lv <guanghai.lv@mihoyo.com>
2019-06-21 19:32:01 +0800

    add

--- Project/MyProject/MyProject/Source/MyProjectEditor/Private/BaseFrame/UnitTest/TestAsset/EditorTestAsset.cpp
+++ Project/MyProject/MyProject/Source/MyProjectEditor/Private/BaseFrame/UnitTest/TestAsset/EditorTestAsset.cpp
@@ -13,66 +13,66 @@
 
 MY_BEGIN_NAMESPACE(MyNS)
 
-void TestAsset::run()
-{
-	this->_testDep();
-}
-
-void TestAsset::_testDep()
-{
-	TArray<FName> OutDependencies;
-	GMyAssetRegistry->GetDependencies(TEXT("Blueprint'/Game/MyAsset/MyTom/PakTest.PakTest'"), OutDependencies);
-}
-
-void TestAsset::_testGetAssetData()
-{
-	// Engine\Plugins\2D\Paper2D\Source\Paper2DEditor\Private\PaperSpriteFactory.cpp
-	// UObject* UPaperSpriteFactory::FactoryCreateNew(UClass* Class, UObject* InParent, FName Name, EObjectFlags Flags, UObject* Context, FFeedbackContext* Warn)
-	FString objectPath = TEXT("");
-
-	FAssetData AssetData = GMyAssetRegistry->GetAssetByObjectPath(*objectPath);
-
-	if (AssetData.IsValid())
-	{
-		if (UTexture2D* NormalMapTexture = Cast<UTexture2D>(AssetData.GetAsset()))
-		{
-
-		}
-	}
-}
-
-// http://www.90cg.com/wordpress/ue4_unreal-asset-loading/
-void TestAsset::_testLoadAndPackageName()
-{
-	{
-		FStringAssetReference testAssetRef = " /Game/UMG/NewWidgetBlueprint ";
-		UObject* pObject = testAssetRef.TryLoad();
-		UWidgetBlueprint* pWidgetBlueprint = Cast<UWidgetBlueprint>(pObject);
-		//error C2664 : 'WidgetT *CreateWidget<UUserWidget,MyNS::TestAsset>(OwnerT *,TSubclassOf<UUserWidget>,FName)' : cannot convert argument 2 from 'TSubclassOf<UObject>' to 'TSubclassOf<UUserWidget>'
-		//auto widget = CreateWidget<UUserWidget>(this, pWidgetBlueprint->GeneratedClass);
-		// error C2664: 'WidgetT *CreateWidget<UUserWidget,MyNS::TestAsset>(OwnerT *,TSubclassOf<UUserWidget>,FName)': cannot convert argument 2 from 'T *' to 'TSubclassOf<UUserWidget>'
-		//auto widget = CreateWidget<UUserWidget>(this, Cast<UUserWidget>(pWidgetBlueprint->GeneratedClass));
-		//auto widget = CreateWidget<UUserWidget>(this, Cast<TSubclassOf<UUserWidget>>(pWidgetBlueprint->GeneratedClass));
-		// do something
-		// widget->AddToViewport();
-		FString pathName1 = FPackageName::LongPackageNameToFilename(pObject->GetPathName(), FPackageName::GetAssetPackageExtension());
-		FString pathName2 = FPackageName::LongPackageNameToFilename(pObject->GetOutermost()->GetPathName(), FPackageName::GetAssetPackageExtension());
-	}
-	{
-		FStringAssetReference testAssetRef = " /Game/UGameAssets/Assets/horizo​​n/Maps/MainMap ";
-		UObject* pObject = testAssetRef.TryLoad();
-		UWorld* pWorld = Cast<UWorld>(pObject);
-		FString pathName1 = FPackageName::LongPackageNameToFilename(pObject->GetPathName(), FPackageName::GetMapPackageExtension());
-		FString pathName2 = FPackageName::LongPackageNameToFilename(pObject->GetOutermost()->GetPathName(), FPackageName::GetMapPackageExtension());
-	}
-	{
-		FStringAssetReference testAssetRef = " /Game/UGameAssets/Assets/horizo​​n/flipbook/hero_down ";
-		UPaperFlipbook* pObject = Cast<UPaperFlipbook>(testAssetRef.TryLoad());
-	}
-	{
-		FStringAssetReference testAssetRef = " /Game/UGameAssets/Assets/horizo​​n/spriter/MyTexture ";
-		UTexture2D* pObject = Cast<UTexture2D>(testAssetRef.TryLoad());
-	}
-}
+//void TestAsset::run()
+//{
+//	this->_testDep();
+//}
+//
+//void TestAsset::_testDep()
+//{
+//	TArray<FName> OutDependencies;
+//	GMyAssetRegistry->GetDependencies(TEXT("Blueprint'/Game/MyAsset/MyTom/PakTest.PakTest'"), OutDependencies);
+//}
+//
+//void TestAsset::_testGetAssetData()
+//{
+//	// Engine\Plugins\2D\Paper2D\Source\Paper2DEditor\Private\PaperSpriteFactory.cpp
+//	// UObject* UPaperSpriteFactory::FactoryCreateNew(UClass* Class, UObject* InParent, FName Name, EObjectFlags Flags, UObject* Context, FFeedbackContext* Warn)
+//	FString objectPath = TEXT("");
+//
+//	FAssetData AssetData = GMyAssetRegistry->GetAssetByObjectPath(*objectPath);
+//
+//	if (AssetData.IsValid())
+//	{
+//		if (UTexture2D* NormalMapTexture = Cast<UTexture2D>(AssetData.GetAsset()))
+//		{
+//
+//		}
+//	}
+//}
+//
+//// http://www.90cg.com/wordpress/ue4_unreal-asset-loading/
+//void TestAsset::_testLoadAndPackageName()
+//{
+//	{
+//		FStringAssetReference testAssetRef = " /Game/UMG/NewWidgetBlueprint ";
+//		UObject* pObject = testAssetRef.TryLoad();
+//		UWidgetBlueprint* pWidgetBlueprint = Cast<UWidgetBlueprint>(pObject);
+//		//error C2664 : 'WidgetT *CreateWidget<UUserWidget,MyNS::TestAsset>(OwnerT *,TSubclassOf<UUserWidget>,FName)' : cannot convert argument 2 from 'TSubclassOf<UObject>' to 'TSubclassOf<UUserWidget>'
+//		//auto widget = CreateWidget<UUserWidget>(this, pWidgetBlueprint->GeneratedClass);
+//		// error C2664: 'WidgetT *CreateWidget<UUserWidget,MyNS::TestAsset>(OwnerT *,TSubclassOf<UUserWidget>,FName)': cannot convert argument 2 from 'T *' to 'TSubclassOf<UUserWidget>'
+//		//auto widget = CreateWidget<UUserWidget>(this, Cast<UUserWidget>(pWidgetBlueprint->GeneratedClass));
+//		//auto widget = CreateWidget<UUserWidget>(this, Cast<TSubclassOf<UUserWidget>>(pWidgetBlueprint->GeneratedClass));
+//		// do something
+//		// widget->AddToViewport();
+//		FString pathName1 = FPackageName::LongPackageNameToFilename(pObject->GetPathName(), FPackageName::GetAssetPackageExtension());
+//		FString pathName2 = FPackageName::LongPackageNameToFilename(pObject->GetOutermost()->GetPathName(), FPackageName::GetAssetPackageExtension());
+//	}
+//	{
+//		FStringAssetReference testAssetRef = " /Game/UGameAssets/Assets/horizo​​n/Maps/MainMap ";
+//		UObject* pObject = testAssetRef.TryLoad();
+//		UWorld* pWorld = Cast<UWorld>(pObject);
+//		FString pathName1 = FPackageName::LongPackageNameToFilename(pObject->GetPathName(), FPackageName::GetMapPackageExtension());
+//		FString pathName2 = FPackageName::LongPackageNameToFilename(pObject->GetOutermost()->GetPathName(), FPackageName::GetMapPackageExtension());
+//	}
+//	{
+//		FStringAssetReference testAssetRef = " /Game/UGameAssets/Assets/horizo​​n/flipbook/hero_down ";
+//		UPaperFlipbook* pObject = Cast<UPaperFlipbook>(testAssetRef.TryLoad());
+//	}
+//	{
+//		FStringAssetReference testAssetRef = " /Game/UGameAssets/Assets/horizo​​n/spriter/MyTexture ";
+//		UTexture2D* pObject = Cast<UTexture2D>(testAssetRef.TryLoad());
+//	}
+//}
 
 MY_END_NAMESPACE
--- Project/MyProject/MyProject/Source/MyProjectEditor/Private/BaseFrame/UnitTest/TestAsset/EditorTestAsset.h
+++ Project/MyProject/MyProject/Source/MyProjectEditor/Private/BaseFrame/UnitTest/TestAsset/EditorTestAsset.h
@@ -7,14 +7,14 @@ MY_BEGIN_NAMESPACE(MyNS)
 
 class TestAsset : public TestBase
 {
-public:
-	virtual void run() override;
-
-protected:
-	void _testDep();
-	void _testGetAssetData();
-	// http://www.90cg.com/wordpress/ue4_unreal-asset-loading/
-	void _testLoadAndPackageName();
+//public:
+//	virtual void run() override;
+//
+//protected:
+//	void _testDep();
+//	void _testGetAssetData();
+//	// http://www.90cg.com/wordpress/ue4_unreal-asset-loading/
+//	void _testLoadAndPackageName();
 };
 
 MY_END_NAMESPACE
