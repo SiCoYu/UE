65cbe92f1033fb348c5726cf9cdcb5c319ca160c
guanghai.lv <guanghai.lv@mihoyo.com>
2019-07-01 20:34:37 +0800

    change

--- Project/MyProject/MyProject/Source/MyProject/Private/MyEngine/MyBlueprintGeneratedClass/MyBlueprintGeneratedClass.h
+++ Project/MyProject/MyProject/Source/MyProject/Private/MyEngine/MyBlueprintGeneratedClass/MyBlueprintGeneratedClass.h
@@ -1,30 +1,11 @@
 #pragma once
 
-#include "GameFramework/Actor.h"
-#include "MyActorBase.generated.h"
+#include "PlatformDefine.h"
 
-UCLASS(config=Game)
-class AMyActorBase : public AActor
+class UBlueprintGeneratedClass;
+
+class MyBlueprintGeneratedClass
 {
-	GENERATED_BODY()
-
-private:
-	FString mName;
-
-public:
-	// 缺少这个，子类构造报错，error C2664: 'AMyActorBase::AMyActorBase(AMyActorBase &&)': cannot convert argument 1 from 'const FObjectInitializer' to 'FVTableHelper &'
-	AMyActorBase(const FObjectInitializer& ObjectInitializer);
-	AMyActorBase();
-	virtual ~AMyActorBase();
-
-public:
-	virtual void NotifyActorOnClicked(FKey ButtonPressed = EKeys::LeftMouseButton) override;
-
-	void SetInfo(FString name);
-
-	// ue4-控制台执行方法: http://blog.csdn.net/yangxuan0261/article/details/53969564
-	UFUNCTION(Exec)
-	void execInConsole();
-
-	virtual void NotifyHit(class UPrimitiveComponent* MyComp, AActor* Other, class UPrimitiveComponent* OtherComp, bool bSelfMoved, FVector HitLocation, FVector HitNormal, FVector NormalImpulse, const FHitResult& Hit) override;
+protected:
+	UBlueprintGeneratedClass* mBlueprintGeneratedClass;
 };
